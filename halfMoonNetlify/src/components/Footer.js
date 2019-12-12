27987e7d7c5b36c6020e99d079ac50f1fef48afc
Rich Dominguez <RRJDominguez@gmail.com>
2019-12-12 12:00:28 -0500

    adding react player

--- halfMoonNetlify/src/components/Footer.js
+++ halfMoonNetlify/src/components/Footer.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
 
-import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import logo from '../img/logo.svg';
+import facebook from '../img/social/facebook.svg';
+import instagram from '../img/social/instagram.svg';
+import twitter from '../img/social/twitter.svg';
+import vimeo from '../img/social/vimeo.svg';
+
+import halfMoonLogoBig from '../img/Halfmoon-3.png';
 
 const Footer = class extends React.Component {
   render() {
@@ -13,7 +15,7 @@ const Footer = class extends React.Component {
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
           <img
-            src={logo}
+            src={halfMoonLogoBig}
             alt="Kaldi"
             style={{ width: '14em', height: '10em' }}
           />
@@ -49,8 +51,7 @@ const Footer = class extends React.Component {
                         className="navbar-item"
                         href="/admin/"
                         target="_blank"
-                        rel="noopener noreferrer"
-                      >
+                        rel="noopener noreferrer">
                         Admin
                       </a>
                     </li>
@@ -108,8 +109,8 @@ const Footer = class extends React.Component {
           </div>
         </div>
       </footer>
-    )
+    );
   }
-}
+};
 
-export default Footer
+export default Footer;
--- halfMoonNetlify/src/components/Navbar.js
+++ halfMoonNetlify/src/components/Navbar.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from 'react';
+import { Link } from 'gatsby';
+import github from '../img/github-icon.svg';
+import logo from '../img/logo.svg';
+import halfMoonIcon from '../img/Halfmoon-7.png';
 
 const Navbar = class extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
       navBarActiveClass: '',
-    }
+    };
   }
 
   toggleHamburger = () => {
@@ -27,29 +28,31 @@ const Navbar = class extends React.Component {
             })
           : this.setState({
               navBarActiveClass: '',
-            })
+            });
       }
-    )
-  }
+    );
+  };
 
   render() {
     return (
       <nav
         className="navbar is-transparent"
         role="navigation"
-        aria-label="main-navigation"
-      >
+        aria-label="main-navigation">
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img
+                src={halfMoonIcon}
+                alt="Kaldi"
+                style={{ width: '100%', backgroundColor: 'black' }}
+              />
             </Link>
             {/* Hamburger menu */}
             <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
               data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
+              onClick={() => this.toggleHamburger()}>
               <span />
               <span />
               <span />
@@ -57,11 +60,10 @@ const Navbar = class extends React.Component {
           </div>
           <div
             id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
+            className={`navbar-menu ${this.state.navBarActiveClass}`}>
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                About Us
               </Link>
               <Link className="navbar-item" to="/products">
                 Products
@@ -81,8 +83,7 @@ const Navbar = class extends React.Component {
                 className="navbar-item"
                 href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
                 target="_blank"
-                rel="noopener noreferrer"
-              >
+                rel="noopener noreferrer">
                 <span className="icon">
                   <img src={github} alt="Github" />
                 </span>
@@ -91,8 +92,8 @@ const Navbar = class extends React.Component {
           </div>
         </div>
       </nav>
-    )
+    );
   }
-}
+};
 
-export default Navbar
+export default Navbar;

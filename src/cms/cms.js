1a6d09fb2a4056df2962437a9b5871edf3e0e4c7
Rich Dominguez <RRJDominguez@gmail.com>
2020-01-15 11:51:58 -0500

    initial adding of events to cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,11 +5,13 @@ import BlogPostPreview from './preview-templates/BlogPostPreview';
 import ProductPagePreview from './preview-templates/ProductPagePreview';
 import IndexPagePreview from './preview-templates/IndexPagePreview';
 import DjPagePreview from './preview-templates/DjPagePreview';
+import EventPagePreview from './preview-templates/EventPagePreview';
 
 CMS.registerPreviewTemplate('index', IndexPagePreview);
 CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('products', ProductPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
 CMS.registerPreviewTemplate('djs', DjPagePreview);
+CMS.registerPreviewTemplate('events', EventPagePreview);
 
 //

2112bf3d5b8a2938d6bf93be3a30426656ba81aa
Christian Mejia <chrismejia10@gmail.com>
2020-03-05 14:01:55 -0500

    Add News Page prelim-layout; vertical mobile layout testing

--- src/components/two-bar/NewsPage.js
+++ src/components/two-bar/NewsPage.js
@@ -4,6 +4,17 @@ function NewsPage() {
   return (
     <section className="news-page container is-fluid">
       <div className="columns is-multiline">
+        <div className="column is-6 news">
+          <div className="news-info">
+            <p>CATEGORY</p>
+            <p>Article Title</p>
+            <p>Article Tagline</p>
+            <div className="article-info">
+              <p>Author Name</p>
+              <p>Article Date</p>
+            </div>
+          </div>
+        </div>
         <div className="column is-12 news">is-12</div>
         <div className="column is-6 news">is-6</div>
         <div className="column is-6 news">is-6</div>

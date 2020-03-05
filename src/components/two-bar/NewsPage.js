a15925f7dd7e780ece774bb042a3fd58a69f822e
Christian Mejia <chrismejia10@gmail.com>
2020-03-05 16:47:09 -0500

    Add NewsPage mobile accordion

--- src/components/two-bar/NewsPage.js
+++ src/components/two-bar/NewsPage.js
@@ -1,28 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewsPage() {
+  const [isOpen, setIsOpen] = useState("");
+
+  function toggleDrawer(e) {
+    if (isOpen === e.currentTarget.id) {
+      setIsOpen("");
+    } else {
+      setIsOpen(e.currentTarget.id);
+    }
+  }
   return (
-    <section className="news-page container is-fluid">
-      <div className="columns is-multiline">
-        <div className="column is-6 news">
-          <div className="news-info">
-            <p>CATEGORY</p>
-            <p>Article Title</p>
-            <p>Article Tagline</p>
-            <div className="article-info">
-              <p>Author Name</p>
-              <p>Article Date</p>
-            </div>
+    <div className="news-hero">
+      <div id="div-1" onClick={toggleDrawer}>
+        <p className="title is-1">div-1</p>
+      </div>
+      {isOpen === "div-1" ? (
+        <div className="white-content container is-fluid">
+          <div className="columns" id="columns-mobile">
+            <div className="column is-12 img-nature">Stuff</div>
+            <div className="column is-12 img-nature">Stuff</div>
+            <div className="column is-12 img-nature">Stuff</div>
+            <div className="column is-12 img-nature">Stuff</div>
+            <div className="column is-12 img-nature">Stuff</div>
+            <div className="column is-12 img-nature">Stuff</div>
           </div>
         </div>
-        <div className="column is-12 news">is-12</div>
-        <div className="column is-6 news">is-6</div>
-        <div className="column is-6 news">is-6</div>
-        <div className="column is-6 news">is-6</div>
-        <div className="column is-6 news">is-6</div>
-        <div className="column is-6 news">is-6</div>
+      ) : (
+        <div className="white-content is-hidden">you can't see me</div>
+      )}
+      <div id="div-2" onClick={toggleDrawer}>
+        <p className="title is-1">div-2</p>
       </div>
-    </section>
+      {isOpen === "div-2" ? (
+        <div className="white-content container is-fluid">
+          <div className="columns" id="columns-mobile">
+            <div className="column is-12 img-food">Stuff</div>
+            <div className="column is-12 img-food">Stuff</div>
+            <div className="column is-12 img-food">Stuff</div>
+            <div className="column is-12 img-food">Stuff</div>
+            <div className="column is-12 img-food">Stuff</div>
+            <div className="column is-12 img-food">Stuff</div>
+          </div>
+        </div>
+      ) : (
+        <div className="white-content is-hidden">you can't see me</div>
+      )}
+      <div id="div-3" onClick={toggleDrawer}>
+        <p className="title is-1">div-3</p>
+      </div>
+      {isOpen === "div-3" ? (
+        <div className="white-content container is-fluid">
+          <div className="columns" id="columns-mobile">
+            <div className="column is-12 img-cars">Stuff</div>
+            <div className="column is-12 img-cars">Stuff</div>
+            <div className="column is-12 img-cars">Stuff</div>
+            <div className="column is-12 img-cars">Stuff</div>
+            <div className="column is-12 img-cars">Stuff</div>
+            <div className="column is-12 img-cars">Stuff</div>
+          </div>
+        </div>
+      ) : (
+        <div className="white-content is-hidden">you can't see me</div>
+      )}
+    </div>
   );
 }
 

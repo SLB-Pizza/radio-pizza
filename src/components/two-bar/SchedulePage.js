14726384a9e7cbfc96f053a2e62d048ad1d8a9c5
Christian Mejia <chrismejia10@gmail.com>
2020-03-09 07:23:39 -0400

    Make mobile date picker scrollable horizontally

--- src/components/two-bar/SchedulePage.js
+++ src/components/two-bar/SchedulePage.js
@@ -4,147 +4,19 @@ import React from "react";
  * STRUCTURE
  *
  * schedule-page container
- * ---- 'columns' div for day picker
- * ---- 'columns' divs for schedule entry
- * ----- each of these columns contains one show's info
- * ------ could setup contditional render for:
- * ------ tagline, halfmoon bio page, twitter, instagram
+ * - 'columns' div for day picker
+ * - 'columns' divs for schedule entry
+ * --- each of these columns contains one show's info
+ * ---- could setup contditional render for:
+ * ---- tagline, halfmoon bio page, twitter, instagram
  */
 
 function SchedulePage() {
   return (
     <div className="container is-fluid schedule-page">
       {/*
-      DESKTOP
+      DESKTOP -- 769px and up
       */}
-      <div className="columns">
-        {/*
-          DAYS OF WEEK
-          */}
-        <div className="column time-date has-text-centered">
-          <p className="title is-3">MON</p>
-          <p className="subtitle is-5">3/16</p>
-        </div>
-        <div className="column time-date has-text-centered">
-          <p className="title is-3">TUE</p>
-          <p className="subtitle is-5">3/17</p>
-        </div>
-        <div className="column time-date has-text-centered">
-          <p className="title is-3">WED</p>
-          <p className="subtitle is-5">3/18</p>
-        </div>
-        <div className="column time-date has-text-centered">
-          <p className="title is-3">THU</p>
-          <p className="subtitle is-5">3/19</p>
-        </div>
-        <div className="column time-date has-text-centered">
-          <p className="title is-3">FRI</p>
-          <p className="subtitle is-5">3/20</p>
-        </div>
-        <div className="column time-date has-text-centered">
-          <p className="title is-3">SAT</p>
-          <p className="subtitle is-5">3/21</p>
-        </div>
-        <div className="column time-date has-text-centered">
-          <p className="title is-3">SUN</p>
-          <p className="subtitle is-5">3/22</p>
-        </div>
-      </div>
-      {/*
-        SCHEDULE ENTRIES
-        */}
-      <div className="columns is-vcentered">
-        <div className="column is-3">
-          <p className="is-size-4 has-text-centered">1:30PM - 4:00PM</p>
-        </div>
-        <div className="column is-narrow">
-          <p className="is-size-4">Short Show</p>
-        </div>
-        <div className="column is-narrow">
-          <p>bio</p>
-        </div>
-        <div className="column is-narrow">
-          <p>twitter</p>
-        </div>
-        <div className="column is-narrow">
-          <p>instagram</p>
-        </div>
-      </div>
-
-      <div className="columns is-vcentered">
-        <div className="column is-3">
-          <p className="is-size-4 has-text-centered">4:00PM - 5:45PM</p>
-        </div>
-        <div className="column is-narrow">
-          <p className="is-size-4">Longer Show Name</p>
-        </div>
-        <div className="column is-narrow">
-          <p>bio</p>
-        </div>
-        <div className="column is-narrow">
-          <p>twitter</p>
-        </div>
-        <div className="column is-narrow">
-          <p>instagram</p>
-        </div>
-      </div>
-
-      <div className="columns is-vcentered">
-        <div className="column is-3">
-          <p className="is-size-4 has-text-centered">5:45PM - 7:00PM</p>
-        </div>
-        <div className="column is-narrow">
-          <p className="is-size-4">Medium Show Name</p>
-        </div>
-        <div className="column is-narrow">
-          <p>bio</p>
-        </div>
-        <div className="column is-narrow">
-          <p>twitter</p>
-        </div>
-        <div className="column is-narrow">
-          <p>instagram</p>
-        </div>
-      </div>
-
-      <div className="columns is-vcentered">
-        <div className="column is-3">
-          <p className="is-size-4 has-text-centered">7:00PM - 10:00PM</p>
-        </div>
-        <div className="column">
-          <p className="is-size-4">The Show with the Long Long Name</p>
-          <p className="is-size-6">Show Host</p>
-        </div>
-        <div className="column is-narrow">
-          <p>bio</p>
-        </div>
-        <div className="column is-narrow">
-          <p>twitter</p>
-        </div>
-        <div className="column is-narrow">
-          <p>instagram</p>
-        </div>
-      </div>
-      <div className="columns is-vcentered">
-        <div className="column is-3">
-          <p className="is-size-4 has-text-centered">10:00PM - 1:30AM</p>
-        </div>
-        <div className="column is-narrow">
-          <p className="is-size-4">Late Night Show</p>
-        </div>
-        <div className="column is-narrow">
-          <p>bio</p>
-        </div>
-        <div className="column is-narrow">
-          <p>twitter</p>
-        </div>
-        <div className="column is-narrow">
-          <p>instagram</p>
-        </div>
-      </div>
-
-      {/*
-      MOBILE */}
       <div className="columns is-hidden-touch">
         {/*
           DAYS OF WEEK
@@ -254,6 +126,138 @@ function SchedulePage() {
         </div>
       </div>
       <div className="columns is-vcentered is-hidden-touch">
+        <div className="column is-3">
+          <p className="is-size-4 has-text-centered">10:00PM - 1:30AM</p>
+        </div>
+        <div className="column is-narrow">
+          <p className="is-size-4">Late Night Show</p>
+        </div>
+        <div className="column is-narrow">
+          <p>bio</p>
+        </div>
+        <div className="column is-narrow">
+          <p>twitter</p>
+        </div>
+        <div className="column is-narrow">
+          <p>instagram</p>
+        </div>
+      </div>
+
+      {/*
+      MOBILE -- < 768px
+      */}
+      <div
+        className="columns is-mobile is-hidden-desktop"
+        id="mobile-date-picker"
+      >
+        {/*
+          DAYS OF WEEK
+          */}
+        <div className="column is-5 time-date has-text-centered">
+          <p className="title is-4">MON</p>
+          <p className="subtitle is-6">3/16</p>
+        </div>
+        <div className="column is-5 time-date has-text-centered">
+          <p className="title is-4">TUE</p>
+          <p className="subtitle is-6">3/17</p>
+        </div>
+        <div className="column is-5 time-date has-text-centered">
+          <p className="title is-4">WED</p>
+          <p className="subtitle is-6">3/18</p>
+        </div>
+        <div className="column is-5 time-date has-text-centered">
+          <p className="title is-4">THU</p>
+          <p className="subtitle is-6">3/19</p>
+        </div>
+        <div className="column is-5 time-date has-text-centered">
+          <p className="title is-4">FRI</p>
+          <p className="subtitle is-6">3/20</p>
+        </div>
+        <div className="column is-5 time-date has-text-centered">
+          <p className="title is-4">SAT</p>
+          <p className="subtitle is-6">3/21</p>
+        </div>
+        <div className="column is-5 time-date has-text-centered">
+          <p className="title is-4">SUN</p>
+          <p className="subtitle is-6">3/22</p>
+        </div>
+      </div>
+      {/*
+        SCHEDULE ENTRIES
+        */}
+      <div className="columns is-vcentered is-hidden-desktop">
+        <div className="column is-3">
+          <p className="is-size-4 has-text-centered">1:30PM - 4:00PM</p>
+        </div>
+        <div className="column is-narrow">
+          <p className="is-size-4">Short Show</p>
+        </div>
+        <div className="column is-narrow">
+          <p>bio</p>
+        </div>
+        <div className="column is-narrow">
+          <p>twitter</p>
+        </div>
+        <div className="column is-narrow">
+          <p>instagram</p>
+        </div>
+      </div>
+
+      <div className="columns is-vcentered is-hidden-desktop">
+        <div className="column is-3">
+          <p className="is-size-4 has-text-centered">4:00PM - 5:45PM</p>
+        </div>
+        <div className="column is-narrow">
+          <p className="is-size-4">Longer Show Name</p>
+        </div>
+        <div className="column is-narrow">
+          <p>bio</p>
+        </div>
+        <div className="column is-narrow">
+          <p>twitter</p>
+        </div>
+        <div className="column is-narrow">
+          <p>instagram</p>
+        </div>
+      </div>
+
+      <div className="columns is-vcentered is-hidden-desktop">
+        <div className="column is-3">
+          <p className="is-size-4 has-text-centered">5:45PM - 7:00PM</p>
+        </div>
+        <div className="column is-narrow">
+          <p className="is-size-4">Medium Show Name</p>
+        </div>
+        <div className="column is-narrow">
+          <p>bio</p>
+        </div>
+        <div className="column is-narrow">
+          <p>twitter</p>
+        </div>
+        <div className="column is-narrow">
+          <p>instagram</p>
+        </div>
+      </div>
+
+      <div className="columns is-vcentered is-hidden-desktop">
+        <div className="column is-3">
+          <p className="is-size-4 has-text-centered">7:00PM - 10:00PM</p>
+        </div>
+        <div className="column">
+          <p className="is-size-4">The Show with the Long Long Name</p>
+          <p className="is-size-6">Show Host</p>
+        </div>
+        <div className="column is-narrow">
+          <p>bio</p>
+        </div>
+        <div className="column is-narrow">
+          <p>twitter</p>
+        </div>
+        <div className="column is-narrow">
+          <p>instagram</p>
+        </div>
+      </div>
+      <div className="columns is-vcentered is-hidden-desktop">
         <div className="column is-3">
           <p className="is-size-4 has-text-centered">10:00PM - 1:30AM</p>
         </div>

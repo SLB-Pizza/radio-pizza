b520b6341d46c3f47a3f9e8ae232b9865b432151
Christian Mejia <chrismejia10@gmail.com>
2020-03-09 01:06:06 -0400

    Add base layout to SchedulePage; hero with dates; time and show names section

--- src/components/two-bar/SchedulePage.js
+++ src/components/two-bar/SchedulePage.js
@@ -2,9 +2,65 @@ import React from "react";
 
 function SchedulePage() {
   return (
-    <section className="schedule-page container is-fluid">
-      <div className="columns">
-        <div className="column">column</div>
+    <section className="schedule-page">
+      <div className="hero schedule-hero">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-multiline">
+              <div className="column is-12">
+                <p className="title is-1">Schedule</p>
+              </div>
+              <div className="column time-date">
+                <p className="title is-5 has-text-centered">
+                  Today
+                  <p className="subtitle is-6">March 16th</p>
+                </p>
+              </div>
+              <div className="column time-date">
+                <p className="title is-5 has-text-centered">
+                  Tuesday
+                  <p className="subtitle is-6">March 17th</p>
+                </p>
+              </div>
+              <div className="column time-date">
+                <p className="title is-5 has-text-centered">
+                  Wednesday
+                  <p className="subtitle is-6">March 18th</p>
+                </p>
+              </div>
+              <div className="column time-date">
+                <p className="title is-5 has-text-centered">
+                  Thursday
+                  <p className="subtitle is-6">March 19th</p>
+                </p>
+              </div>
+              <div className="column time-date">
+                <p className="title is-5 has-text-centered">
+                  Friday
+                  <p className="subtitle is-6">March 20th</p>
+                </p>
+              </div>
+              <div className="column time-date">
+                <p className="title is-5 has-text-centered">
+                  Saturday
+                  <p className="subtitle is-6">March 21st</p>
+                </p>
+              </div>
+              <div className="column time-date">
+                <p className="title is-5 has-text-centered">
+                  Sunday
+                  <p className="subtitle is-6">March 22nd</p>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="columns is-vcentered">
+          <div className="column is-4">img</div>
+          <div className="column is-8">times</div>
+        </div>
       </div>
     </section>
   );

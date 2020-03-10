dc3388fdd6b2c1568fac70fbf3629c3f232b7710
Christian Mejia <chrismejia10@gmail.com>
2020-03-10 05:26:03 -0400

    Add CategoryItem to last push

---
+++ src/components/two-bar/CategoryItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function CategoryItem() {
+  return (
+    <div className="cat-item">
+      <div className="item-info">
+        <p className="item-date is-size-7">March 30</p>
+        <p className="is-size-3 has-text-centered">Lorem Ipsum</p>
+        <p className="is-size-6 has-text-centered">
+          Lorem ipsum dolor sit amet con secte tur ado manis cheriamus recto
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default CategoryItem;

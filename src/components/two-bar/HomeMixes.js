07370e10b7c40661eefd58209a7ce84655933737
Christian Mejia <chrismejia10@gmail.com>
2020-03-14 20:01:01 -0400

    Make home content section blurb sticky; begin mix layout

--- src/components/two-bar/HomeMixes.js
+++ src/components/two-bar/HomeMixes.js
@@ -9,16 +9,23 @@ function HomeMixes() {
       */}
       <div className="columns">
         <div className="column is-3">
-          <p className="title is-size-2-desktop is-size-4-touch">Radio Sets</p>
-          <p className="title is-size-4-desktop is-size-6-touch">
-            Recent mixes from Halfmoon residents.
-          </p>
+          <div className="sticky-section-blurb">
+            <p className="title is-size-2-desktop is-size-5-touch">
+              Radio Sets
+            </p>
+            <p className="title is-size-4-desktop is-size-7-touch">
+              Recent mixes from Halfmoon residents.
+            </p>
+          </div>
         </div>
         <div className="column is-9">
           <div className="columns is-multiline">
             {/* <div className="column">
               <p>Hello</p>
             </div> */}
+            <HomeSingleMix />
+            <HomeSingleMix />
+            <HomeSingleMix />
             <HomeSingleMix />
             <HomeSingleMix />
             <HomeSingleMix />
--- src/components/two-bar/HomeSingleMix.js
+++ src/components/two-bar/HomeSingleMix.js
@@ -3,8 +3,21 @@ import React from "react";
 function HomeSingleMix() {
   return (
     <div className="column is-4">
-      <div className="mixes-bg">
-        <p className="title is-size-4">Mixes</p>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-16by9">
+            <img
+              src="https://source.unsplash.com/1600x900/?nature"
+              alt="mix-img"
+            />
+          </figure>
+        </div>
+      </div>
+      <div className="card-content">
+        <p className="title is-size-4-desktop is-size-6-touch">
+          Lorem Ipsum Doloris
+        </p>
+        <p className="mix-category is-size-7">CATEGORY</p>
       </div>
     </div>
   );

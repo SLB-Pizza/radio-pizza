cffe9055e2f7a66bbbc0b42061605b19ee970a83
Christian Mejia <chrismejia10@gmail.com>
2020-03-15 20:10:12 -0400

    Add prelim HomeMixes & HomeSingleMix touch layout

--- src/components/two-bar/HomeMixes.js
+++ src/components/two-bar/HomeMixes.js
@@ -7,7 +7,7 @@ function HomeMixes() {
       {/*
       Desktop Sizes
       */}
-      <div className="columns">
+      <div className="columns is-hidden-touch">
         <div className="column is-3">
           <div className="sticky-section-blurb">
             <p className="title is-size-2">Radio Sets</p>
@@ -31,6 +31,32 @@ function HomeMixes() {
             <HomeSingleMix />
           </div>
         </div>
+      </div>
+      {/*
+      Touch Sizes
+      */}
+      <div className="columns is-mobile is-hidden-desktop">
+        <div className="columns is-mobile is-hidden-desktop">
+          <div className="column is-12">
+            <p className="title is-size-2">Radio Sets</p>
+            <p className="title is-size-4">
+              Recent mixes from Halfmoon residents.
+            </p>
+            <button className="button is-small is-dark">
+              View More Mixes >
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="columns is-mobile is-hidden-desktop mobile-mixes">
+        <HomeSingleMix />
+        <HomeSingleMix />
+        <HomeSingleMix />
+        <HomeSingleMix />
+        <HomeSingleMix />
+        <HomeSingleMix />
+        <HomeSingleMix />
+        <HomeSingleMix />
       </div>
     </div>
   );

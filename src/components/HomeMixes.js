b63aa3b5458abcb8e108abe480f8643d55a03986
Christian Mejia <chrismejia10@gmail.com>
2020-03-24 06:55:43 -0400

    Begin home content bg-image black overlay test

--- src/components/HomeMixes.js
+++ src/components/HomeMixes.js
@@ -4,57 +4,59 @@ import { HomeSingleMix } from "./index";
 function HomeMixes() {
   return (
     <div className="container is-fluid" id="home-mixes">
-      {/*
+      <div className="image-diffuser">
+        {/*
       Desktop Sizes
       */}
-      <div className="columns is-hidden-touch">
-        <div className="column is-3">
-          <div className="sticky-section-blurb">
-            <p className="title is-size-2">Daily Mixes</p>
-            <p className="title is-size-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-            <button className="sticky-link button">More ></button>
+        <div className="columns is-hidden-touch">
+          <div className="column is-3">
+            <div className="sticky-section-blurb">
+              <p className="title is-size-2">Daily Mixes</p>
+              <p className="title is-size-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+              <button className="sticky-link button">More ></button>
+            </div>
+          </div>
+          <div className="column is-9">
+            <div className="columns is-multiline">
+              <HomeSingleMix />
+              <HomeSingleMix />
+              <HomeSingleMix />
+              <HomeSingleMix />
+              <HomeSingleMix />
+              <HomeSingleMix />
+              <HomeSingleMix />
+              <HomeSingleMix />
+            </div>
           </div>
         </div>
-        <div className="column is-9">
-          <div className="columns is-multiline">
-            <HomeSingleMix />
-            <HomeSingleMix />
-            <HomeSingleMix />
-            <HomeSingleMix />
-            <HomeSingleMix />
-            <HomeSingleMix />
-            <HomeSingleMix />
-            <HomeSingleMix />
-          </div>
-        </div>
-      </div>
-      {/*
+        {/*
       Touch Sizes
       */}
-      <div className="columns is-mobile is-multiline is-vcentered is-hidden-desktop">
-        <div className="column">
-          <p className="title is-size-2 mobile-headers">Daily Mixes</p>
+        <div className="columns is-mobile is-multiline is-vcentered is-hidden-desktop">
+          <div className="column">
+            <p className="title is-size-2 mobile-headers">Daily Mixes</p>
+          </div>
+          <div className="column is-narrow more-link">
+            <button className="button is-small">More ></button>
+          </div>
+          <div className="column is-12">
+            <p className="title is-size-4 mobile-headers">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
         </div>
-        <div className="column is-narrow more-link">
-          <button className="button is-small">More ></button>
+        <div className="columns is-mobile is-hidden-desktop mobile-single-items">
+          <HomeSingleMix />
+          <HomeSingleMix />
+          <HomeSingleMix />
+          <HomeSingleMix />
+          <HomeSingleMix />
+          <HomeSingleMix />
+          <HomeSingleMix />
+          <HomeSingleMix />
         </div>
-        <div className="column is-12">
-          <p className="title is-size-4 mobile-headers">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-        </div>
-      </div>
-      <div className="columns is-mobile is-hidden-desktop mobile-single-items">
-        <HomeSingleMix />
-        <HomeSingleMix />
-        <HomeSingleMix />
-        <HomeSingleMix />
-        <HomeSingleMix />
-        <HomeSingleMix />
-        <HomeSingleMix />
-        <HomeSingleMix />
       </div>
     </div>
   );
--- src/components/HomeNews.js
+++ src/components/HomeNews.js
@@ -4,45 +4,47 @@ import { HomeSingleNews } from "./index";
 function HomeNews() {
   return (
     <div className="container is-fluid" id="home-news">
-      {/*
+      <div className="image-diffuser">
+        {/*
       Desktop Sizes
       */}
-      <div className="columns is-hidden-touch">
-        <div className="column is-3">
-          <div className="sticky-section-blurb">
-            <p className="title is-size-2">Features</p>
-            <p className="title is-size-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-            <button className="sticky-link button">More ></button>
+        <div className="columns is-hidden-touch">
+          <div className="column is-3">
+            <div className="sticky-section-blurb">
+              <p className="title is-size-2">Features</p>
+              <p className="title is-size-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+              <button className="sticky-link button">More ></button>
+            </div>
+          </div>
+          <div className="column is-9">
+            <div className="columns is-multiline">
+              <HomeSingleNews />
+              <HomeSingleNews />
+            </div>
           </div>
         </div>
-        <div className="column is-9">
-          <div className="columns is-multiline">
-            <HomeSingleNews />
-            <HomeSingleNews />
-          </div>
-        </div>
-      </div>
-      {/*
+        {/*
       Touch Sizes
       */}
-      <div className="columns is-mobile is-multiline is-vcentered is-hidden-desktop">
-        <div className="column">
-          <p className="title is-size-2 mobile-headers">Features</p>
+        <div className="columns is-mobile is-multiline is-vcentered is-hidden-desktop">
+          <div className="column">
+            <p className="title is-size-2 mobile-headers">Features</p>
+          </div>
+          <div className="column is-narrow more-link">
+            <button className="button is-small">More ></button>
+          </div>
+          <div className="column is-12">
+            <p className="title is-size-4 mobile-headers">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
         </div>
-        <div className="column is-narrow more-link">
-          <button className="button is-small">More ></button>
+        <div className="columns is-mobile is-hidden-desktop mobile-single-items">
+          <HomeSingleNews />
+          <HomeSingleNews />
         </div>
-        <div className="column is-12">
-          <p className="title is-size-4 mobile-headers">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-        </div>
-      </div>
-      <div className="columns is-mobile is-hidden-desktop mobile-single-items">
-        <HomeSingleNews />
-        <HomeSingleNews />
       </div>
     </div>
   );

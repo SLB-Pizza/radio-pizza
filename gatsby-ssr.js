73fc728bcb7c63b3d5e945f9b724b7863fac1b10
Rich Dominguez <RRJDominguez@gmail.com>
2020-03-26 16:29:26 -0400

    debugging srr vs broswer apis

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,12 @@
-// const React = require('react');
-// const GlobalContextProvider = require('./src/context/GlobalContextProvider')
-//   .default;
+const React = require('react');
+const GlobalContextProvider = require('./src/context/GlobalContextProvider')
+  .default;
 
 // const Layout = require('./src/components/Layout').default;
 
-// exports.wrapRootElement = ({ element }) => {
-//   return <GlobalContextProvider>{element}</GlobalContextProvider>;
-// };
+exports.wrapRootElement = ({ element }) => {
+  return <GlobalContextProvider>{element}</GlobalContextProvider>;
+};
 
 // exports.wrapPageElement = ({ element, props }) => {
 //   return <Layout {...props}>{element}</Layout>;

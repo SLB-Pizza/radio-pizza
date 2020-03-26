28e5c09f6258c9ee94f1f8cca60c715f5ac04f13
Rich Dominguez <RRJDominguez@gmail.com>
2020-03-26 16:21:37 -0400

    debugging gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import GlobalContextProvider from './src/context/GlobalContextProvider';
-import Layout from './src/components/Layout';
+// import React from 'react';
+// import GlobalContextProvider from './src/context/GlobalContextProvider';
+// import Layout from './src/components/Layout';
 
-export const wrapRootElement = ({ element }) => {
-  return <GlobalContextProvider>{element}</GlobalContextProvider>;
-};
+// export const wrapRootElement = ({ element }) => {
+//   return <GlobalContextProvider>{element}</GlobalContextProvider>;
+// };
 
-// Wraps every page in a component
-export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>;
-};
+// // Wraps every page in a component
+// export const wrapPageElement = ({ element, props }) => {
+//   return <Layout {...props}>{element}</Layout>;
+// };

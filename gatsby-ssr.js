52d447746d4b607422ecaf20205f733c6a1ec3db
Rich Dominguez <RRJDominguez@gmail.com>
2020-03-26 16:14:31 -0400

    debugging .default in ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,7 +2,7 @@ const React = require('react');
 const GlobalContextProvider = require('./src/context/GlobalContextProvider')
   .default;
 
-const Layout = require('./src/components/Layout');
+const Layout = require('./src/components/Layout').default;
 
 exports.wrapRootElement = ({ element }) => {
   return <GlobalContextProvider>{element}</GlobalContextProvider>;

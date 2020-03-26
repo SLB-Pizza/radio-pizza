837c0cd17bffeca24e542248913ee53779de26c0
Christian Mejia <chrismejia10@gmail.com>
2020-03-26 14:57:04 -0400

    Push to share for debugging deploy

--- src/components/SingleResident.js
+++ src/components/SingleResident.js
@@ -5,10 +5,39 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function SingleResident() {
   return (
     <div className="column is-one-third single-resident">
-      <div className="columns is-vcentered is-mobile">
+      {/*
+      Desktop
+      */}
+      <div className="columns is-vcentered is-hidden-touch">
         <div className="column is-narrow">
           <div className="resident-image">
             <figure className="image is-96x96">
+              <img
+                className="is-rounded"
+                src="https://robohash.org/4H1.png?set=set1"
+                alt="Rowdy Robo"
+              />
+            </figure>
+          </div>
+        </div>
+        <div className="column">
+          <div className="resident-info">
+            <p className="resident-name title is-size-3-desktop is-size-5-touch">
+              RowdyRobo
+              <span className="icon is-large">
+                <FontAwesomeIcon icon={faTwitter} size="sm" />
+              </span>
+              <span className="icon is-large">
+                <FontAwesomeIcon icon={faInstagram} size="sm" />
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="columns is-vcentered is-mobile is-hidden-desktop">
+        <div className="column is-narrow">
+          <div className="resident-image">
+            <figure className="image is-48x48">
               <img
                 className="is-rounded"
                 src="https://robohash.org/4H1.png?set=set1"

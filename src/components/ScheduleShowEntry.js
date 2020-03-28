c856e037bb474b845c5229197841856114ece34c
Christian Mejia <chrismejia10@gmail.com>
2020-03-27 21:41:59 -0400

    Fix font-sizing and short-screen mixin for wider screens

--- src/components/ScheduleShowEntry.js
+++ src/components/ScheduleShowEntry.js
@@ -5,15 +5,17 @@ function ScheduleShowEntry() {
     <div className="column is-12 entry-container">
       <div className="columns is-mobile is-vcentered">
         <div className="column is-3">
-          <p className="title is-size-5 is-size-6-widescreen has-text-centered">
+          <p className="title is-size-5-desktop is-size-6-touch has-text-centered">
             12:00P - 12:00A
           </p>
         </div>
         <div className="column is-9">
-          <p className="title is-size-5 is-size-6-widescreen">
+          <p className="title is-size-5-desktop is-size-6-touch">
             A Show Name Long Enough to Rival Some Post-Rock Album Titles
           </p>
-          <p className="subtitle is-size-6 is-size-7-widescreen">Host Name</p>
+          <p className="subtitle is-size-6-desktop is-size-7-touch">
+            Host Name
+          </p>
         </div>
       </div>
     </div>

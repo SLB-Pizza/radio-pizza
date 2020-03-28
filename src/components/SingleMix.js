23a24039c765240e485f7362fff32b0bc1d3acff
Christian Mejia <chrismejia10@gmail.com>
2020-03-28 19:49:59 -0400

    Begin hover experimentation with front-page items; bring success to bio mixes

--- src/components/SingleMix.js
+++ src/components/SingleMix.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function SingleMix() {
   return (
-    <div className="column is-6">
+    <div className="column is-4-desktop is-6-touch">
       <div className="single-mix">
         <div className="black-overlay is-flex">
           <div className="mix-info">
@@ -10,15 +10,15 @@ function SingleMix() {
             <p className="title is-size-3-desktop is-size-5-touch">
               Lorem Ipsum Doloris
             </p>
+            <div className="tags are-small">
+              <span className="tag is-primary">Genre</span>
+              <span className="tag is-primary">Genrerock</span>
+              <span className="tag is-primary">Alt-Genre</span>
+              <span className="tag is-primary">Genrecore</span>
+              <span className="tag is-primary">Post-Genre</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="tags are-small">
-        <span className="tag is-primary">Genre</span>
-        <span className="tag is-primary">Genrerock</span>
-        <span className="tag is-primary">Alt-Genre</span>
-        <span className="tag is-primary">Genrecore</span>
-        <span className="tag is-primary">Post-Genre</span>
       </div>
     </div>
   );

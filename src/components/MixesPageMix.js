cd976ac2cbf6148b5a9678435085b0186b64550b
Christian Mejia <chrismejia10@gmail.com>
2020-03-27 22:28:24 -0400

    Add bio page base and bring in single-mix from daily-mixes

---
+++ src/components/MixesPageMix.js
@@ -0,0 +1,25 @@
+// https://localhost:8000/daily-mixes old-single
+
+// import React from "react";
+
+// function SingleMix() {
+//   return (
+//     <div className="column is-6">
+//       <div className="single-mix">
+//         <div className="black-overlay is-flex">
+//           <div className="mix-info">
+//             <p className="mix-category is-size-7">CATEGORY</p>
+//             <p className="title is-size-3-desktop is-size-5-touch">
+//               Lorem Ipsum Doloris
+//             </p>
+//             <p className="subtitle is-size-5-desktop is-size-7-touch">
+//               Lorem ipsum dolor sit amet con secte tur ado manis cheriamus recto
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default SingleMix;
--- src/pages/resident/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ResidentIndexPage() {
-  return <div>Hello</div>;
-}
-
-export default ResidentIndexPage;

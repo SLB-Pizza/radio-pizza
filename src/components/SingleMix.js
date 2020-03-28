f61fcf95a1c43522abb6e16031544b8631b05572
Christian Mejia <chrismejia10@gmail.com>
2020-03-27 22:41:23 -0400

    Add tags group to bio/mixes section

--- src/components/SingleMix.js
+++ src/components/SingleMix.js
@@ -6,15 +6,19 @@ function SingleMix() {
       <div className="single-mix">
         <div className="black-overlay is-flex">
           <div className="mix-info">
-            <p className="mix-category is-size-7">CATEGORY</p>
+            <p className="mix-category title is-size-7">03.29.20</p>
             <p className="title is-size-3-desktop is-size-5-touch">
               Lorem Ipsum Doloris
             </p>
-            <p className="subtitle is-size-5-desktop is-size-7-touch">
-              Lorem ipsum dolor sit amet con secte tur ado manis cheriamus recto
-            </p>
           </div>
         </div>
+      </div>
+      <div className="tags are-small">
+        <span className="tag is-primary">Genre</span>
+        <span className="tag is-primary">Genrerock</span>
+        <span className="tag is-primary">Alt-Genre</span>
+        <span className="tag is-primary">Genrecore</span>
+        <span className="tag is-primary">Post-Genre</span>
       </div>
     </div>
   );

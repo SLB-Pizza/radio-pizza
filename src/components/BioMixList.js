eba45d02d8618da79009e8d58ee8712bf5b2e212
Christian Mejia <chrismejia10@gmail.com>
2020-03-29 23:15:42 -0400

    Recreate resident-mix on bio page; begin adding play btn overlay

--- src/components/BioMixList.js
+++ src/components/BioMixList.js
@@ -13,19 +13,57 @@ function BioMixList(props) {
       <BioSingleMix />
       <BioSingleMix />
       <BioSingleMix />
-      <BioSingleMix />
-      <BioSingleMix />
-      <BioSingleMix />
-      <BioSingleMix />
-      <BioSingleMix />
-      <BioSingleMix />
-      <BioSingleMix />
-      <BioSingleMix />
-      <BioSingleMix />
-      <BioSingleMix />
-      <div className="column is-12">
+
+      <div className="column is-12 is-hidden-touch">
         <nav
-          className="pagination is-small is-centered is-rounded"
+          className="pagination is-centered"
+          role="navigation"
+          aria-label="pagination"
+        >
+          <a className="pagination-previous">Previous</a>
+          <a className="pagination-next">Next page</a>
+          <ul className="pagination-list">
+            <li>
+              <a className="pagination-link" aria-label="Goto page 1">
+                1
+              </a>
+            </li>
+            <li>
+              <span className="pagination-ellipsis">&hellip;</span>
+            </li>
+            <li>
+              <a className="pagination-link" aria-label="Goto page 45">
+                45
+              </a>
+            </li>
+            <li>
+              <a
+                className="pagination-link is-current"
+                aria-label="Page 46"
+                aria-current="page"
+              >
+                46
+              </a>
+            </li>
+            <li>
+              <a className="pagination-link" aria-label="Goto page 47">
+                47
+              </a>
+            </li>
+            <li>
+              <span className="pagination-ellipsis">&hellip;</span>
+            </li>
+            <li>
+              <a className="pagination-link" aria-label="Goto page 86">
+                86
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="column is-12 is-hidden-desktop">
+        <nav
+          className="pagination is-centered"
           role="navigation"
           aria-label="pagination"
         >
--- src/components/BioSingleMix.js
+++ src/components/BioSingleMix.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
 
 function BioSingleMix(props) {
-  return <div>SingleMix</div>;
+  return (
+    <div className="column is-6-touch is-3-desktop">
+      <div className="resident-mix">
+        <div className="image is-1by1">
+          <div className="play-btn-diffuser">
+            <span className="icon is-large">
+              <FontAwesomeIcon icon={faPlay} size="3x" />
+            </span>
+          </div>
+        </div>
+        <div className="mix-content">
+          <p className="content-date is-size-7 is-size-6-fullhd">03.30.20</p>
+          <p className="title is-size-5-mobile is-size-4-tablet is-size-3-fullhd">
+            Lorem Ipsum Dolor
+          </p>
+          <p className="subtitle is-size-7-mobile is-size-6-tablet is-size-5-fullhd">
+            Resident Artist
+          </p>
+          <div className="tags are-small">
+            <span className="tag is-dark">Genre</span>
+            <span className="tag is-dark">Genrerock</span>
+            <span className="tag is-dark">Alt-Genre</span>
+            <span className="tag is-dark">Genrecore</span>
+            <span className="tag is-dark">Post-Genre</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BioSingleMix;

f17e17b7c33d59092a2d213e2ac261cfabd0a83f
Christian Mejia <chrismejia10@gmail.com>
2020-04-10 17:23:42 -0400

    Change HomeMixes font sizes; make .tag, .card global styles; increase card border width

--- src/components/HomeSingleMix.js
+++ src/components/HomeSingleMix.js
@@ -14,18 +14,18 @@ function HomeSingleMix() {
         </div>
         <div className="card-content">
           <p className="content-date is-size-7">03.30.20</p>
-          <p className="title is-size-5-mobile is-size-4-tablet is-size-3-fullhd">
+          <p className="title is-size-6-mobile is-size-5-tablet is-size-4-fullhd">
             Lorem Ipsum Dolor
           </p>
           <p className="subtitle is-size-7-mobile is-size-6-tablet is-size-5-fullhd">
             Resident Artist
           </p>
           <div className="tags are-small">
-            <span className="tag is-dark">Genre</span>
-            <span className="tag is-dark">Genrerock</span>
-            <span className="tag is-dark">Alt-Genre</span>
-            <span className="tag is-dark">Genrecore</span>
-            <span className="tag is-dark">Post-Genre</span>
+            <span className="tag is-black">Genre</span>
+            <span className="tag is-black">Genrerock</span>
+            <span className="tag is-black">Alt-Genre</span>
+            <span className="tag is-black">Genrecore</span>
+            <span className="tag is-black">Post-Genre</span>
           </div>
         </div>
       </div>

a6a83aafcced04da594bf754adc3b6a236455da5
Christian Mejia <chrismejia10@gmail.com>
2020-04-13 19:04:12 -0400

    Remove old format single resident code

--- src/components/SingleResident.js
+++ src/components/SingleResident.js
@@ -5,21 +5,6 @@ function SingleResident(props) {
   return (
     <div className="column is-4-widescreen is-6-tablet is-12-mobile resident-box">
       <Link to="/bio">
-        {/* <div className="columns is-vcentered is-mobile is-multiline single-resident">
-          <div className="column is-12 resident-image">
-            <figure className="image is-2by1">
-              <img
-                src="https://source.unsplash.com/1280x640/weekly?cyberpunk"
-                alt="Rowdy Robo"
-              />
-            </figure>
-          </div>
-          <div className="column is-12">
-            <p className="title is-size-5-desktop is-size-7-touch">
-              {props.name}
-            </p>
-          </div>
-        </div> */}
         <div className="card">
           <div className="card-image">
             <figure className="image is-2by1">

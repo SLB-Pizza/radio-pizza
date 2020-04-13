90e016c994f660b01d2e3f26fa45106a33c98ad8
Christian Mejia <chrismejia10@gmail.com>
2020-04-13 18:15:43 -0400

    Change single-resident layout to 16x9 img and text; add border w/hover

--- src/components/SingleResident.js
+++ src/components/SingleResident.js
@@ -5,42 +5,17 @@ function SingleResident(props) {
   return (
     <div className="column is-4-widescreen is-6-tablet is-12-mobile resident-box">
       <Link to="/bio">
-        {/*
-      Desktop
-      */}
-        <div className="columns is-vcentered is-hidden-touch single-resident">
-          <div className="column is-narrow">
-            <div className="resident-image">
-              <figure className="image is-96x96">
-                <img
-                  src="https://robohash.org/4H1.png?set=set1"
-                  alt="Rowdy Robo"
-                />
-              </figure>
-            </div>
+        <div className="columns is-vcentered is-mobile is-multiline single-resident">
+          <div className="column is-12 resident-image">
+            <figure className="image is-2by1">
+              <img
+                src="https://source.unsplash.com/1280x640/weekly?cyberpunk"
+                alt="Rowdy Robo"
+              />
+            </figure>
           </div>
-          <div className="column">
-            <p className="title is-size-4-desktop is-size-6-touch">
-              {props.name}
-            </p>
-          </div>
-        </div>
-        {/*
-      TOUCH
-      */}
-        <div className="columns is-vcentered is-mobile is-hidden-desktop single-resident">
-          <div className="column is-narrow">
-            <div className="resident-image">
-              <figure className="image is-64x64">
-                <img
-                  src="https://robohash.org/4H1.png?set=set1"
-                  alt="Rowdy Robo"
-                />
-              </figure>
-            </div>
-          </div>
-          <div className="column">
-            <p className="title is-size-4-desktop is-size-6-touch">
+          <div className="column is-12">
+            <p className="title is-size-5-desktop is-size-7-touch">
               {props.name}
             </p>
           </div>

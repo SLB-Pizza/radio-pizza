589b630daad8aad7bb473f565ca2129a2d531201
Christian Mejia <chrismejia10@gmail.com>
2020-04-13 18:36:37 -0400

    Change single resident container code to standard card

--- src/components/SingleResident.js
+++ src/components/SingleResident.js
@@ -5,7 +5,7 @@ function SingleResident(props) {
   return (
     <div className="column is-4-widescreen is-6-tablet is-12-mobile resident-box">
       <Link to="/bio">
-        <div className="columns is-vcentered is-mobile is-multiline single-resident">
+        {/* <div className="columns is-vcentered is-mobile is-multiline single-resident">
           <div className="column is-12 resident-image">
             <figure className="image is-2by1">
               <img
@@ -16,6 +16,21 @@ function SingleResident(props) {
           </div>
           <div className="column is-12">
             <p className="title is-size-5-desktop is-size-7-touch">
+              {props.name}
+            </p>
+          </div>
+        </div> */}
+        <div className="card">
+          <div className="card-image">
+            <figure className="image is-2by1">
+              <img
+                src="https://source.unsplash.com/1280x640/weekly?cyberpunk"
+                alt="mix-img"
+              />
+            </figure>
+          </div>
+          <div className="card-content">
+            <p className="title is-size-6-mobile is-size-5-tablet is-size-4-fullhd">
               {props.name}
             </p>
           </div>

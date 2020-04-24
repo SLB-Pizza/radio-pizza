6de66ad7284f7347ca905e53781ee79a2f99a5c7
Christian Mejia <chrismejia10@gmail.com>
2020-04-24 18:36:50 -0400

    fix (layout): add bottom padding to Bio
    In addition to padding, a centered, split <hr /> with logo was added under the mix list
    closes #77

--- src/pages/bio/index.js
+++ src/pages/bio/index.js
@@ -3,11 +3,23 @@ import { BioImageBlurb, BioMixList } from "../../components/";
 
 function ResidentBioIndexPage() {
   return (
-    <div className="resident-bio">
-      <div className="container is-fluid">
-        <BioImageBlurb />
-        <hr />
-        <BioMixList />
+    <div className="container is-fluid resident-bio">
+      <BioImageBlurb />
+      <hr />
+      <BioMixList />
+      <div className="columns is-mobile is-vcentered">
+        <div className="column">
+          <hr />
+        </div>
+        <div className="column is-narrow">
+          <figure className="image is-64x64">
+            <img src={`../../img/halfmoon-3.png`} alt="HalfmoonBK logo" />
+          </figure>
+        </div>
+
+        <div className="column">
+          <hr />
+        </div>
       </div>
     </div>
   );

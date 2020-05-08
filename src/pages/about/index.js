712f18850a984436ac419ece7e239935e235a1ef
Christian Mejia <chrismejia10@gmail.com>
2020-05-08 19:46:26 -0400

    feat: add about page copy from Surf's text

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -10,14 +10,18 @@ function AboutIndexPage() {
         <div className="columns is-multiline">
           <div className="column is-12">
             <div className="content">
-              <p className="title is-3">Ears to the concrete.</p>
+              <p className="title is-size-3">Ears to the concrete.</p>
+              <p className="subtitle is-size-5">
+                Brooklyn based music network connecting music culture across the
+                world through radio, events, and more.
+              </p>
               <hr />
-              <p className="subtitle is-4">Team</p>
+              <p className="subtitle is-size-4">Team</p>
               <p>Surf Allah – Owner & Founder</p>
               <p style={{ marginBottom: "2.5rem" }}>
                 Edrick Chu – Head of Marketing & Partnerships
               </p>
-              <p className="subtitle is-4">Contact</p>
+              <p className="subtitle is-size-6">Contact</p>
               <p>Business: business@halfmoonbk.com</p>
               <p>Info: info@halfmoonbk.com</p>
             </div>

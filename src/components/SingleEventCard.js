61d063509626cca2cb2f6fea1e73d6d88543bf02
Christian Mejia <chrismejia10@gmail.com>
2020-06-13 15:39:09 -0400

    style: mobile mix cards - reduce spacing between title and tags
    update #97

--- src/components/SingleEventCard.js
+++ src/components/SingleEventCard.js
@@ -11,16 +11,14 @@ function SingleEventCard(props) {
         </figure>
       </div>
       <div className="card-content">
-        <div className="content-text">
-          <p className="content-date subtitle is-size-7-touch is-size-7-desktop is-size-6-widescreen">
+        <div className="event-text">
+          <p className="content-date subtitle is-size-7">
             {props.date} | {props.location}
           </p>
           <p className="title is-size-6-touch is-size-6-desktop is-size-5-widescreen">
             {props.eventName}
           </p>
-          <p className="is-size-6-touch is-size-6-desktop is-size-5-widescreen">
-            {props.blurb}
-          </p>
+          <p className="is-size-6-touch is-size-6-desktop">{props.blurb}</p>
         </div>
       </div>
     </div>

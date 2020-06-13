db254ed750c56290b92c3a9894d0580d98435e49
Christian Mejia <chrismejia10@gmail.com>
2020-06-13 12:42:03 -0400

    layout: /about - add new base layout

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,38 +2,36 @@ import React from "react";
 
 function AboutIndexPage() {
   return (
-    <section className="about-page">
-      <div className="about-pic">
-        <img src="../../img/Halfmoon-3.png" alt="HalfMoonBK Logo" />
-      </div>
-      <div className="container is-fluid about-info">
-        <div className="columns is-multiline">
-          <div className="column is-12">
-            <div className="content">
-              <p className="title is-size-3">Ears to the concrete.</p>
-              <p className="subtitle is-size-5">
-                Brooklyn based music network connecting music culture across the
-                world through radio, events, and more.
-              </p>
-              <hr />
-              <p className="title is-size-5">Team</p>
-              <p>Surf Allah – Owner & Founder</p>
-              <p style={{ marginBottom: "2.5rem" }}>
-                Edrick Chu – Head of Marketing & Partnerships
-              </p>
-              <p className="title is-size-5">Contact</p>
-              <p>Business: business@halfmoonbk.com</p>
-              <p style={{ marginBottom: "2.5rem" }}>
-                Info: info@halfmoonbk.com
-              </p>
-              <p className="title is-size-5">Site</p>
-              <p>Designed by Christian Mejia</p>
-              <p>Programmed by Christian Mejia & Richard Dominguez</p>
-            </div>
+    <div className="container is-fluid site-page">
+      <div className="columns is-vcentered" id="about">
+        <div className="column is-3">
+          <figure className="image is-9by16">
+            <img src="https://source.unsplash.com/720x1280/daily?soundboard" />
+          </figure>
+        </div>
+        <div className="column is-9">
+          <div className="content">
+            <p className="title is-size-3">Ears to the concrete.</p>
+            <p className="subtitle is-size-5">
+              Brooklyn based music network connecting music culture across the
+              world through radio, events, and more.
+            </p>
+            <hr />
+            <p className="title is-size-5">Team</p>
+            <p>Surf Allah – Owner & Founder</p>
+            <p style={{ marginBottom: "2.5rem" }}>
+              Edrick Chu – Head of Marketing & Partnerships
+            </p>
+            <p className="title is-size-5">Contact</p>
+            <p>Business: business@halfmoonbk.com</p>
+            <p style={{ marginBottom: "2.5rem" }}>Info: info@halfmoonbk.com</p>
+            <p className="title is-size-5">Site</p>
+            <p>Designed by Christian Mejia</p>
+            <p>Programmed by Christian Mejia & Richard Dominguez</p>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
 

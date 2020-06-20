ea88dc44caed8aa28c6e9becbe4c6c16934805c4
Christian Mejia <chrismejia10@gmail.com>
2020-06-19 21:30:43 -0400

    test(page): install react-responsive-carousel; create base /hero-test

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,39 +3,51 @@ import Slider from "react-slick";
 
 function Hero(props) {
   let settings = {
-    dots: true,
+    adaptiveHeight: true,
+    dots: false,
+    fade: true,
     infinite: true,
+    lazyLoad: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    swipeToSlide: true,
   };
   return (
     <div className="homepage-hero">
       <div className="container is-fluid">
         <Slider {...settings}>
-          <div>
-            <img
-              src="https://source.unsplash.com/1920x1080/daily?coffee"
-              alt="coffee"
-            />
+          <div className="columns">
+            <div className="column">
+              <img
+                src="https://source.unsplash.com/1920x1080/daily?coffee"
+                alt="coffee"
+              />
+            </div>
           </div>
-          <div>
-            <img
-              src="https://source.unsplash.com/1920x1080/daily?headphones"
-              alt="headphones"
-            />
+          <div className="columns">
+            <div className="column">
+              <img
+                src="https://source.unsplash.com/1920x1080/daily?headphones"
+                alt="headphones"
+              />
+            </div>
           </div>
-          <div>
-            <img
-              src="https://source.unsplash.com/1920x1080/daily?concert"
-              alt="concert"
-            />
+          <div className="columns">
+            <div className="column">
+              <img
+                src="https://source.unsplash.com/1920x1080/daily?concert"
+                alt="concert"
+              />
+            </div>
           </div>
-          <div>
-            <img
-              src="https://source.unsplash.com/1920x1080/daily?musician"
-              alt="musician"
-            />
+          <div className="columns">
+            <div className="column">
+              <img
+                src="https://source.unsplash.com/1920x1080/daily?musician"
+                alt="musician"
+              />
+            </div>
           </div>
         </Slider>
       </div>
---
+++ src/pages/hero-test/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HeroTest() {
+  return (
+    <div className="container is-fluid site-content">
+      <div className="columns is-mobile is-multiline">
+        <div className="column is-full">Text</div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroTest;

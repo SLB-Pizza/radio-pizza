5423a2d356024e6c656ee6578fbea544c18dfd5b
Christian Mejia <chrismejia10@gmail.com>
2020-07-06 23:53:30 -0400

    feat: remove play button from SingleMixCard
    closes #113; sr-only link tags place inside img - currently working

--- src/components/SingleMixCard.js
+++ src/components/SingleMixCard.js
@@ -8,55 +8,6 @@ function SingleMixCard(props) {
   const dispatch = useContext(GlobalDispatchContext);
 
   /**
-   * @function playAudioButton - function that takes in props from BioMixList and creates active YT audio sources
-   * @returns {jsx} A play icon that onClick dispatches the CHANGE_URL action, playing the audio source through RadioPlayer.js
-   * @param {Object[]} buttons - the array of button object details to create functional mix play buttons
-   * @param {string} buttons.size - the size of the play button represented from 1x - 10x
-   * @param {string} buttons.url - the audio url
-   * @param {string} buttons.title - title of the audio
-   * @param {string} [buttons.viewportClass] - className to attach to the play button
-   */
-
-  const playAudioButton = (url, title, artist, img) => {
-    return (
-      <span>
-        <FontAwesomeIcon
-          icon={faPlay}
-          size="5x"
-          onClick={() =>
-            dispatch({
-              type: "CHANGE_URL",
-              payload: {
-                url: url,
-                title: title,
-                artist: artist,
-                img: img,
-              },
-            })
-          }
-        >
-          <a
-            href="#"
-            className="sr-only is-overlay"
-            onClick={() =>
-              dispatch({
-                type: "CHANGE_URL",
-                payload: {
-                  url: url,
-                  title: title,
-                  artist: artist,
-                },
-              })
-            }
-          >
-            Play This Mix
-          </a>
-        </FontAwesomeIcon>
-      </span>
-    );
-  };
-
-  /**
    * imageAltText for accessibility purposes
    */
   const imageAltText = `image - ${props.name} by ${props.artist}`;
@@ -64,12 +15,40 @@ function SingleMixCard(props) {
   return (
     <div className={props.columnLayout}>
       <div className="card">
-        <div className="card-image">
+        <div
+          className="card-image"
+          onClick={() =>
+            dispatch({
+              type: "CHANGE_URL",
+              payload: {
+                url: props.url,
+                title: props.title,
+                artist: props.artist,
+                img: props.img,
+              },
+            })
+          }
+        >
+          <a
+            href="#"
+            className="sr-only display-text"
+            onClick={() =>
+              dispatch({
+                type: "CHANGE_URL",
+                payload: {
+                  url: props.url,
+                  title: props.title,
+                  artist: props.artist,
+                  img: props.img,
+                },
+              })
+            }
+          >
+            Play This Mix
+          </a>
           <figure className="image is-1by1">
             <img src={props.img} alt={imageAltText} />
-            <div className="play-btn-diffuser is-overlay">
-              {playAudioButton(props.url, props.name, props.artist, props.img)}
-            </div>
+            <div className="play-btn-diffuser is-overlay" />
           </figure>
         </div>
         <div className="card-content">

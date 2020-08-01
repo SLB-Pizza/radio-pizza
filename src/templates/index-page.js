562765104e95b71b40a480295dbd61a6918b2cba
Christian Mejia <chrismejia10@gmail.com>
2020-08-01 16:13:06 -0400

    "fix: fix broken HomepageQuery; log data to console"

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -6,46 +6,48 @@ import "../styles/index.scss";
 import { Hero, HomeContent } from "../components";
 
 function IndexPageTemplate({ data }) {
+  console.log("Homepage Query", data);
+
   // Focus the node for the prismicContent check below.
-  const prismicContent = data.prismic.allHomepages.edges[0];
+  // const prismicContent = data.prismic.allHomepages.edges[0];
   /**
    * This line is here to prevent an error from occurring when you eventually deploy the site live. There is an issue with the preview functionality that requires this check on every page.
    * Details: https://prismic.io/docs/gatsby/rendering/retrieve-the-document-object#21_0-adding-a-validation-check
    */
-  if (!prismicContent) return null;
+  // if (!prismicContent) return null;
 
   // Grab the data object from prismicContent
-  const document = prismicContent.node;
+  // const document = prismicContent.node;
 
   // Structure the node's data to pass as props
-  const slidesForHero = document.homepage_carousel;
+  // const slidesForHero = document.homepage_carousel;
 
   return (
     <div className="has-navbar-fixed-bottom site-page">
-      <Hero slides={slidesForHero} />
+      {/* <Hero slides={slidesForHero}/> */}
       <HomeContent />
     </div>
   );
 }
 
 export const query = graphql`
-  query HomepageQuery {
+  {
     prismic {
       allHomepages {
         edges {
           node {
+            _linkType
             homepage_carousel {
-              layout_bg_image
-              slide_headline
+              slide_bg_url
               slide_cta
               slide_link {
-                _linkType
                 ... on PRISMIC__ExternalLink {
                   target
                   _linkType
                   url
                 }
               }
+              slide_headline
             }
           }
         }

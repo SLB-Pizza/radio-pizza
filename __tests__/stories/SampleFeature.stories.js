885ff1f680d5b01ee5eedd462b01e7851d10d7ba
Christian Mejia <chrismejia10@gmail.com>
2020-08-05 16:51:29 -0400

    fix: repair Sample Feature story

--- __tests__/stories/SampleFeature.stories.js
+++ __tests__/stories/SampleFeature.stories.js
@@ -7,7 +7,30 @@ export default {
 };
 
 const fwiBgUrl = "https://w.wallhaven.cc/full/lq/wallhaven-lqwgdy.jpg";
-const bgType = "none";
+const blockquoteSlice = {
+  primary: {
+    blockquote_type: "None: no background image; white quote text on black",
+    blockquote_text: [
+      {
+        type: "paragraph",
+        text:
+          "We are made of star stuff. We are a way for the cosmos to know itself.",
+        spans: [],
+      },
+    ],
+    blockquote_attribution: [
+      {
+        type: "paragraph",
+        text: "Carl Sagan, Cosmos",
+        spans: [],
+      },
+    ],
+    blockquote_bg_img: {
+      url:
+        "https://images.prismic.io/hmbk-cms/874d566f-66e0-491f-ab1c-3372019385f1_wallhaven-yjm67g_1920x1200.png?auto=compress,format",
+    },
+  },
+};
 
 export const SampleFeature = () => (
   <>
@@ -16,6 +39,6 @@ export const SampleFeature = () => (
       data={"Medium Full Width Image"}
       bgUrl={fwiBgUrl}
     />
-    <Blockquote bgType={bgType} />
+    <Blockquote slice={blockquoteSlice} />
   </>
 );

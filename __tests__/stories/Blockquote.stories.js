41633844b407f72d74fc2d8949b330f9a137420e
Christian Mejia <chrismejia10@gmail.com>
2020-08-04 22:40:49 -0400

    feat: add long TwoParagraph story

--- __tests__/stories/Blockquote.stories.js
+++ __tests__/stories/Blockquote.stories.js
@@ -83,6 +83,37 @@ const darkBGSlice = {
   },
 };
 
+const twoParagraphSlice = {
+  primary: {
+    blockquote_type: "None: no background image; white quote text on black",
+    blockquote_text: [
+      {
+        type: "paragraph",
+        text:
+          "For many years, I have been moved by the blue at the far edge of what can be seen, that color of horizons, of remote mountain ranges, of anything far away. The color of that distance is the color of an emotion, the color of solitude and of desire, the color of there seen from here, the color of where you are not. And the color of where you can never go. For the blue is not in the place those miles away at the horizon, but in the atmospheric distance between you and the mountains.",
+        spans: [],
+      },
+      {
+        type: "paragraph",
+        text:
+          "“Longing,” says the poet Robert Hass, “because desire is full of endless distances.” Blue is the color of longing for the distances you never arrive in, for the blue world.”",
+        spans: [],
+      },
+    ],
+    blockquote_attribution: [
+      {
+        type: "paragraph",
+        text: "Rebecca Solnit, A Field Guide to Getting Lost",
+        spans: [],
+      },
+    ],
+    blockquote_bg_img: {
+      url:
+        "https://images.prismic.io/hmbk-cms/874d566f-66e0-491f-ab1c-3372019385f1_wallhaven-yjm67g_1920x1200.png?auto=compress,format",
+    },
+  },
+};
+
 // =======================
 // Stories
 // =======================
@@ -92,3 +123,5 @@ export const noBG = () => <Blockquote slice={noBGSlice} />;
 export const DarkBG = () => <Blockquote slice={darkBGSlice} />;
 
 export const LightBG = () => <Blockquote slice={lightBGSlice} />;
+
+export const TwoParagraph = () => <Blockquote slice={twoParagraphSlice} />;

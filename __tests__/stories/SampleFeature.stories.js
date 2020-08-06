1bf765e7538969c3809558fa518154f0d0fabf16
Christian Mejia <chrismejia10@gmail.com>
2020-08-06 19:10:54 -0400

    docs: add comment about {sliceZoneContent} being the way it is

--- __tests__/stories/SampleFeature.stories.js
+++ __tests__/stories/SampleFeature.stories.js
@@ -2,12 +2,68 @@ import React from "react";
 import {
   Blockquote,
   FullWidthImage,
+  HeadlineBlock,
   TwoImagesAndText,
 } from "../../src/components/slices";
 import "../../src/styles/index.scss";
 
 export default {
   title: "Prismic CMS Slices/Sample Layouts",
+};
+
+const metadata = {
+  uid: "dev-test-feature-1",
+  firstPublicationDate: "2020-08-02T17:45:25+0000",
+  lastPublicationDate: "2020-08-06T21:25:19+0000",
+  type: "feature",
+  tags: [],
+};
+
+const headlineSlice = {
+  primary: {
+    feature_headline_img: {
+      dimensions: {
+        width: 2880,
+        height: 1800,
+      },
+      alt: "oslo escalator",
+      copyright: null,
+      url:
+        "https://images.prismic.io/hmbk-cms/450e3121-5d28-48eb-961c-937cfd5bd364_wallhaven-w8yxlr.jpg?auto=compress,format",
+    },
+    feature_category: "music",
+    feature_subcategory: "playlists",
+    feature_headline: [
+      {
+        type: "heading1",
+        text: "Beyond 174: The Search for the Ultimate High Speed Workout",
+        spans: [],
+      },
+    ],
+    feature_subtitle: [
+      {
+        type: "heading4",
+        text:
+          "These ten must-add songs will bring your next workout to blistering heights.",
+        spans: [],
+      },
+    ],
+    feature_author_pic: {
+      dimensions: {
+        width: 750,
+        height: 750,
+      },
+      alt: "Gabo",
+      copyright: null,
+      url:
+        "https://images.prismic.io/hmbk-cms/33e0177d-19d2-45f3-b30e-c666552c512e_gabriel-garcia-marquez-2.jpg?auto=compress,format&rect=76,0,750,750&w=750&h=750",
+    },
+    feature_author: {
+      __typename: "PRISMIC_Staff",
+      hmbk_staff_name: "Gabriel Garcia Marquez",
+      hmbk_staff_position: "Nobel Laureate",
+    },
+  },
 };
 
 const fwiBgUrl = "https://w.wallhaven.cc/full/lq/wallhaven-lqwgdy.jpg";
@@ -60,12 +116,13 @@ const twoImageAndTextSlice = {
 
 export const SampleFeature = () => (
   <>
+    <HeadlineBlock slice={headlineSlice} metadata={metadata} />
+    <Blockquote slice={blockquoteSlice} />
     <FullWidthImage
       type={"medium"}
       data={"Medium Full Width Image"}
       bgUrl={fwiBgUrl}
     />
-    <Blockquote slice={blockquoteSlice} />
     <TwoImagesAndText slice={twoImageAndTextSlice} />
   </>
 );
--- src/components/SliceZone.js
+++ src/components/SliceZone.js
@@ -32,3 +32,23 @@ export default function SliceZone({ sliceZone, featureMetadata }) {
 
   return <>{sliceZoneContent}</>;
 }
+
+/**
+ * Why is sliceZoneContent is returned in a fragment and not wrapped in an element?
+ *
+ * The template that has the query for the data to pass as props into SliceZone returns a structure as follows:
+ * <main className="site-page">
+ *    <SliceZone
+        sliceZone={featureSliceData}
+        featureMetadata={featureMetadata}
+      />
+ * </main>
+ *
+ * Each CMS Slice returned by SliceZone is wrapped in a <section> element, so if we did, there would be unnecessary elements between <main> and the <section>'s -> e.g.
+ *
+ * <main className="site-page">
+ *    <div>
+ *      <section>{slice}</section>
+ *    </div>
+ * </main>
+ */

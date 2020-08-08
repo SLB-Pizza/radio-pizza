fce4e0e6375810ecf6ec9be8b1c5efe956e2f3c6
Christian Mejia <chrismejia10@gmail.com>
2020-08-08 13:41:48 -0400

    docs(CMS): add SliceZone jsdoc info

--- src/components/SliceZone.js
+++ src/components/SliceZone.js
@@ -6,6 +6,11 @@ import {
   TwoImagesAndText,
 } from "./slices";
 
+/** *
+ * @function sliceZone - Receives params as props from a template file. Processes the sliceZone object to match slice types, the keys in sliceComponents, to their corresponding component values.
+ * @param {Object} { sliceZone, featureMetadata }
+ * @returns {jsx} Returns components selected by sliceComponents key that have been hydrated with data to create the given page's layout.
+ */
 export default function SliceZone({ sliceZone, featureMetadata }) {
   // Structure the imported Slice Components with their api value
   const sliceComponents = {
@@ -48,7 +53,7 @@ export default function SliceZone({ sliceZone, featureMetadata }) {
  *
  * <main className="site-page">
  *    <div>
- *      <section>{slice}</section>
+ *      <section>{slice layout here}</section>
  *    </div>
  * </main>
  */

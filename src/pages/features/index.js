6b2fd1c9d4bdac4fd727abd04ee5c40391ca8cdc
Christian Mejia <chrismejia10@gmail.com>
2020-08-09 19:49:28 -0400

    fix(features): import graphql via gatsby to address deploy warning

--- src/pages/features/index.js
+++ src/pages/features/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
 import { StickyFeature } from "../../components";
 import PropTypes from "prop-types";
 

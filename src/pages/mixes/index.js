73c44a9118286b771399d2a5d691cce3a7fde037
Christian Mejia <chrismejia10@gmail.com>
2020-08-25 05:00:44 -0400

    refactor(/mixes): rename old mix index; create new

--- src/pages/mixes/index.js
+++ src/pages/mixes/index.js
@@ -1,98 +1,166 @@
-import React, { useState } from "react";
+import React from "react";
+import { graphql } from "gatsby";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faTag } from "@fortawesome/free-solid-svg-icons";
-import { SingleMixCard } from "../../components";
+import { SliceZone, SingleMixCard } from "../../components/";
 
-// Dummy data in __tests__ folder
-import sampleMixes from "../../../__tests__/sampleMixes.json";
+/**
+ * @category Pages
+ * @subcategory Indexes
+ * @function MixesIndex
+ * @param {object} data - the data object coming from Prismic CMS that contains all data needed to display all mixes on `/mixes`
+ * @returns {jsx}
+ */
 
-const dummyOptions = [
-  "Argentina",
-  "Bolivia",
-  "Brazil",
-  "Chile",
-  "Colombia",
-  "Ecuador",
-  "Guyana",
-  "Paraguay",
-  "Peru",
-  "Suriname",
-  "Uruguay",
-  "Venezuela",
-];
-
-function MixesIndexPage() {
-  const [searchInput, setSearchInput] = useState("");
+function MixesIndex({ data }) {
+  /**
+   * **NB:** allMixs is NOT a typo.
+   */
+  const prismicContent = data.prismic.allMixs.edges;
+  if (!prismicContent) return null;
 
   /**
-   * @param {string} mixListLayout - The string that will be used as the className to style this component's SingleMixCard data mapping. This string follows the site's bulma.io column styling conventions.
+   * Grab and manip the nodes array of mixs
    */
+  const allMixesData = prismicContent;
+
+  // const { _meta, body } = leadFeatureData;
+
   const mixListLayout =
     "column is-12-mobile is-6-tablet is-4-desktop is-3-widescreen";
+  const dummyOptions = [
+    "Argentina",
+    "Bolivia",
+    "Brazil",
+    "Chile",
+    "Colombia",
+    "Ecuador",
+    "Guyana",
+    "Paraguay",
+    "Peru",
+    "Suriname",
+    "Uruguay",
+    "Venezuela",
+  ];
 
   return (
-    <div className="container is-fluid site-page">
-      <div className="columns is-mobile is-multiline">
-        <div className="column is-full">
-          <p className="title is-size-3-desktop is-size-4-touch">
-            Recent Mixes
-          </p>
-          <p className="subtitle is-size-5-desktop is-size-6-touch">
-            These dummy mixes are the same as the ones on the home page. You can
-            hover/touch and play them the same way. Try it!
-          </p>
-        </div>
-        <div className="column is-9-widescreen is-8-tablet is-12-mobile">
-          <div className="field">
-            <div className="control is-expanded has-icons-left has-icons-right">
-              {/* <div className="control is-expanded has-icons-left has-icons-right is-loading is-medium"> */}
-              <input
-                className="input is-rounded"
-                type="text"
-                placeholder="Search all mixes..."
-              />
-              <span className="icon is-left">
-                <FontAwesomeIcon icon={faSearch} />
-              </span>
-            </div>
+    <main className="site-page">
+      {/* FIRST SECTION - Header Section */}
+      <header className="container is-fluid">
+        <div className="columns is-mobile is-multiline">
+          <div className="column is-full">
+            <p className="title is-size-3-desktop is-size-4-touch">
+              Recent Mixes
+            </p>
+            <p className="subtitle is-size-5-desktop is-size-6-touch">
+              These dummy mixes are the same as the ones on the home page. You
+              can hover/touch and play them the same way. Try it!
+            </p>
           </div>
-        </div>
-        <div className="column is-3-widescreen is-4-tablet is-12-mobile">
-          <div className="field">
-            <div className="control is-expanded has-icons-left">
-              <div className="select is-fullwidth is-rounded">
-                <select name="country">
-                  <option value="">--Country--</option>
-                  {dummyOptions.map((option) => (
-                    <option key={option} value={option}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
+          <div className="column is-9-widescreen is-8-tablet is-12-mobile">
+            <div className="field">
+              <div className="control is-expanded has-icons-left has-icons-right">
+                {/* <div className="control is-expanded has-icons-left has-icons-right is-loading is-medium"> */}
+                <input
+                  className="input is-rounded"
+                  type="text"
+                  placeholder="Search all mixes..."
+                />
+                <span className="icon is-left">
+                  <FontAwesomeIcon icon={faSearch} />
+                </span>
               </div>
+            </div>
+          </div>
+          <div className="column is-3-widescreen is-4-tablet is-12-mobile">
+            <div className="field">
+              <div className="control is-expanded has-icons-left">
+                <div className="select is-fullwidth is-rounded">
+                  <select name="country">
+                    <option value="">--Country--</option>
+                    {dummyOptions.map((option) => (
+                      <option key={option} value={option}>
+                        {option}
+                      </option>
+                    ))}
+                  </select>
+                </div>
 
-              <span className="icon is-left">
-                <FontAwesomeIcon icon={faTag} />
-              </span>
+                <span className="icon is-left">
+                  <FontAwesomeIcon icon={faTag} />
+                </span>
+              </div>
             </div>
           </div>
         </div>
-        {sampleMixes.map((mix) => (
-          <SingleMixCard
-            key={mix.mixTitle}
-            date={mix.mixDate}
-            url={mix.mixUrl}
-            testSrc={mix.mixTestSrc}
-            title={mix.mixTitle}
-            resident={mix.mixResident}
-            img={mix.mixImg}
-            tags={mix.mixTags}
-            columnLayout={mixListLayout}
-          />
-        ))}
-      </div>
-    </div>
+      </header>
+      {/* SECOND SECTION - Mix section */}
+      <section className="container is-fluid">
+        <div className="columns is-mobile is-multiline">
+          {/* All Mixs data in pulled correctly */}
+          <hr />
+          {allMixesData.map((singleMix, index) => {
+            const {
+              _meta,
+              mix_date,
+              mix_image,
+              mix_link,
+              mix_title,
+              featured_residents,
+            } = singleMix.node;
+
+            return (
+              <SingleMixCard
+                key={`mix-#${index}-${mix_title}`}
+                date={mix_date}
+                url={mix_link}
+                title={mix_title}
+                residents={featured_residents}
+                img={mix_image}
+                tags={_meta.tags}
+                columnLayout={mixListLayout}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </main>
   );
 }
 
-export default MixesIndexPage;
+export const query = graphql`
+  query MixesIndexPage {
+    prismic {
+      allMixs(sortBy: meta_firstPublicationDate_DESC) {
+        edges {
+          node {
+            _meta {
+              uid
+              lastPublicationDate
+              firstPublicationDate
+              type
+              tags
+            }
+            mix_date
+            mix_image
+            mix_link
+            mix_title
+            featured_residents {
+              mix_resident {
+                ... on PRISMIC_Resident {
+                  resident_name
+                  _meta {
+                    uid
+                    type
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default MixesIndex;
---
+++ src/pages/mixes/old-index.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faTag } from "@fortawesome/free-solid-svg-icons";
+import { SingleMixCard } from "../../components";
+
+// Dummy data in __tests__ folder
+import sampleMixes from "../../../__tests__/sampleMixes.json";
+
+const dummyOptions = [
+  "Argentina",
+  "Bolivia",
+  "Brazil",
+  "Chile",
+  "Colombia",
+  "Ecuador",
+  "Guyana",
+  "Paraguay",
+  "Peru",
+  "Suriname",
+  "Uruguay",
+  "Venezuela",
+];
+
+function MixesIndexPage() {
+  const [searchInput, setSearchInput] = useState("");
+
+  /**
+   * @param {string} mixListLayout - The string that will be used as the className to style this component's SingleMixCard data mapping. This string follows the site's bulma.io column styling conventions.
+   */
+  const mixListLayout =
+    "column is-12-mobile is-6-tablet is-4-desktop is-3-widescreen";
+
+  return (
+    <div className="container is-fluid site-page">
+      <div className="columns is-mobile is-multiline">
+        <div className="column is-full">
+          <p className="title is-size-3-desktop is-size-4-touch">
+            Recent Mixes
+          </p>
+          <p className="subtitle is-size-5-desktop is-size-6-touch">
+            These dummy mixes are the same as the ones on the home page. You can
+            hover/touch and play them the same way. Try it!
+          </p>
+        </div>
+        <div className="column is-9-widescreen is-8-tablet is-12-mobile">
+          <div className="field">
+            <div className="control is-expanded has-icons-left has-icons-right">
+              {/* <div className="control is-expanded has-icons-left has-icons-right is-loading is-medium"> */}
+              <input
+                className="input is-rounded"
+                type="text"
+                placeholder="Search all mixes..."
+              />
+              <span className="icon is-left">
+                <FontAwesomeIcon icon={faSearch} />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="column is-3-widescreen is-4-tablet is-12-mobile">
+          <div className="field">
+            <div className="control is-expanded has-icons-left">
+              <div className="select is-fullwidth is-rounded">
+                <select name="country">
+                  <option value="">--Country--</option>
+                  {dummyOptions.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <span className="icon is-left">
+                <FontAwesomeIcon icon={faTag} />
+              </span>
+            </div>
+          </div>
+        </div>
+        {sampleMixes.map((mix) => (
+          <SingleMixCard
+            key={mix.mixTitle}
+            date={mix.mixDate}
+            url={mix.mixUrl}
+            title={mix.mixTitle}
+            residents={mix.mixResident}
+            img={mix.mixImg}
+            tags={mix.mixTags}
+            columnLayout={mixListLayout}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default MixesIndexPage;

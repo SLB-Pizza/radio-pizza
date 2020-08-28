1329cd8d824998d8f9b0f6433610a7d37f13b8fb
Christian Mejia <chrismejia10@gmail.com>
2020-08-28 17:18:40 -0400

    refactor(slice): rewrite FWI slice to use CMS data

--- src/components/slices/FullWidthImage.js
+++ src/components/slices/FullWidthImage.js
@@ -1,48 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-// const FullWidthImage = ({ slice }) => {
-function FullWidthImage(props) {
-  const { type, data, bgUrl } = props;
+/**
+ * @category CMS
+ * @subcategory Slices
+ * @component
+ * @param {Object} slice - data object from Prismic CMS that contains all content data needed to create the HeadlineBlock slice
+ * @param {Object} metadata - data object from Prismic CMS that contains
+ * @returns {jsx}
+ */ function FullWidthImage({ slice }) {
+  const { full_width_image, fwi_titling, fwi_height } = slice.primary;
 
-  let imgStyle = null;
-  if (bgUrl) {
-    imgStyle = {
-      backgroundImage: `url(${bgUrl})`,
-    };
-  }
+  let imgStyle = {
+    backgroundImage: `url(${full_width_image.url})`,
+  };
 
   let heroClassName = "";
-  switch (type) {
-    case "short":
-      heroClassName = "hero";
+  switch (fwi_height) {
+    case "Short":
+      heroClassName = "hero slice";
       break;
-    case "medium":
-      heroClassName = "hero is-medium";
+    case "Medium":
+      heroClassName = "hero is-medium slice";
       break;
-    case "tall":
-      heroClassName = "hero is-large";
+    case "Tall":
+      heroClassName = "hero is-large slice";
+      break;
+    case "Fullheight":
+      heroClassName = "hero homepage-hero slice";
       break;
     default:
-      heroClassName = "hero";
+      heroClassName = "hero slice";
   }
 
   return (
     <section className={heroClassName} style={imgStyle}>
-      <div className="hero-body">
-        <div className="container">
-          <p className="title is-size-3">{data}</p>
-          <h2 className="subtitle">Full Width Image Title</h2>
-        </div>
+      <div className="hero-body container">
+        {fwi_titling ? <h1 className="title is-size-3">{data}</h1> : null}
       </div>
     </section>
   );
 }
 
-FullWidthImage.propTypes = {
-  type: PropTypes.oneOf(["short", "medium", "tall"]),
-  data: PropTypes.string.isRequired,
-  bgUrl: PropTypes.string.isRequired,
-};
+// FullWidthImage.propTypes = {
+//   type: PropTypes.oneOf(["short", "medium", "tall"]),
+//   data: PropTypes.string.isRequired,
+//   full_width_image.url: PropTypes.string.isRequired,
+// };
 
 export default FullWidthImage;

88cd46e3ed9829098c0561d8ce9fe17ee77e6074
Christian Mejia <chrismejia10@gmail.com>
2020-09-09 23:10:23 -0400

    fix(Apollo): unwrap ApolloProvider from TopNav

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect, useContext } from "react";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { PrismicLink } from "apollo-link-prismic";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
@@ -18,47 +16,26 @@ function TopNav() {
 
   useEffect(() => {
     const clock = setInterval(() => {
-      setNYCTime(nycTime.add(1, "s"));
-      setLATime(laTime.add(1, "s"));
-    }, 1000);
+      setNYCTime(nycTime.add(10, "s"));
+      setLATime(laTime.add(10, "s"));
+    }, 10000);
 
     return () => {
       clearInterval(clock);
     };
   });
 
-  /**
-   * Create the Apollo Client and give it our Prismic CMS graphql endpoint
-   * @name ApolloPrismicClient
-   * @see {@link https://www.apollographql.com/docs/react/get-started/#create-a-client|Create a Client}
-   */
-  const client = new ApolloClient({
-    link: PrismicLink({
-      uri: "https://hmbk-cms.prismic.io/graphql",
-    }),
-    cache: new InMemoryCache(),
-  });
-
-  /**
-   * Wrapping the root client with `<ApolloProvider>` allows us to access {@link ApolloPrismicClient} from anywhere in our app. The `<ApolloProvider>` is similar to React's `Context.Provider`.
-   *
-   * @see {@link https://www.apollographql.com/docs/react/get-started/#connect-your-client-to-react|Connect the Client to the Project}
-   * @see {@link https://reactjs.org/docs/context.html#contextprovider|React's Context Provider}
-   */
-
   return (
-    <ApolloProvider client={client}>
-      <div
-        className={
-          globalState.live
-            ? "radio-and-schedule-bar is-live"
-            : "radio-and-schedule-bar"
-        }
-      >
-        <RadioBar nycTime={nycTime} laTime={laTime} />
-        <ScheduleBar timeNow={nycTime} />
-      </div>
-    </ApolloProvider>
+    <div
+      className={
+        globalState.live
+          ? "radio-and-schedule-bar is-live"
+          : "radio-and-schedule-bar"
+      }
+    >
+      <RadioBar nycTime={nycTime} laTime={laTime} />
+      <ScheduleBar timeNow={nycTime} />
+    </div>
   );
 }
 

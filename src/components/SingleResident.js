7e969558210d2c84c1b5c38321133a7703726a6f
Christian Mejia <chrismejia10@gmail.com>
2020-09-28 18:11:38 -0400

    chore(docs): update SingleResident data

--- src/components/SingleResident.js
+++ src/components/SingleResident.js
@@ -2,6 +2,17 @@ import React from "react";
 import { Link } from "gatsby";
 import { linkResolver, ResponsiveImage } from "../utils";
 
+/**
+ * @category Layout Helper
+ * @subcategory Resident
+ * @function SingleResident
+ * @param {Object} resident - Prismic CMS data object containing all data needed to build resident cards that link out to a `/residents/:uid`
+ * @property {Object} _meta - contains the uid and type for use with {@link linkResolver}
+ * @property {String} resident_name - the resident's name
+ * @property {Object} resident_image - contains the resident image's dimensions object, alt text string, copyright string, and url string
+ * @property {String} resident_status - details the resident's status as being of the three: ["Resident", "Guest", "Alumnus"]
+ * @returns {jsx}
+ */
 function SingleResident({ resident }) {
   const { _meta, resident_name, resident_image } = resident;
 

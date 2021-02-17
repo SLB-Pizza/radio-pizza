b98e499762ede8689853d77cc396acf1f30a11ea
Christian Mejia <chrismejia10@gmail.com>
2021-02-17 05:12:56 -0500

    feat(/residents): attach queries to buttons process fetched data

--- src/pages/residents/index.js
+++ src/pages/residents/index.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react'
 import { graphql } from 'gatsby'
-import { useQuery } from '@apollo/client'
+import { useLazyQuery } from '@apollo/client'
 import { SingleResident } from '../../components'
 import {
   GET_MORE_RESIDENTS,
@@ -15,65 +15,156 @@ import {
  * @returns {jsx}
  */
 
-function ResidentsIndex({ data, prismic }) {
+function ResidentsIndex({ data }) {
   const [isOpen, setIsOpen] = useState('Residents')
-  const [categoryStatus, setCategoryStatus] = useState(null)
+  const [categoryLabels, setCategoryLabels] = useState(null)
 
-  const [residentsData, setResidentsData] = useState(null)
-  const [hasMoreRes, setHasMoreRes] = useState({
+  const [residents, setResidents] = useState({
+    data: null,
+    hasMore: null,
+    endCursor: null,
+  })
+  const [alumni, setAlumni] = useState({
+    data: null,
+    hasMore: null,
+    endCursor: null,
+  })
+  const [guests, setGuests] = useState({
+    data: null,
     hasMore: null,
     endCursor: null,
   })
 
-  const [alumniData, setAlumniData] = useState(null)
-  const [guestsData, setGuestsData] = useState(null)
+  /**
+   * Use Apollo's useLazyQuery to allow for a query to run on button press.
+   * One useLazyQuery per resident category
+   * Each individual fetch update is fed to a useEffect
+   * @see {@link https://www.apollographql.com/docs/react/data/queries/#executing-queries-manually| Executing queries manually (useLazyQuery hook)}
+   */
+  const [
+    getMoreResidents,
+    { loading: resLoad, data: residentFetch },
+  ] = useLazyQuery(GET_MORE_RESIDENTS)
 
+  const [
+    getMoreAlumni,
+    { loading: alumLoad, data: alumniFetch },
+  ] = useLazyQuery(GET_MORE_ALUMNI)
+
+  const [
+    getMoreGuests,
+    { loading: guestLoad, data: guestFetch },
+  ] = useLazyQuery(GET_MORE_GUESTS)
+
+  /**
+   * Don't render this component with having prismicContent
+   */
   const prismicContent = data.prismic
   if (!prismicContent) return null
 
   useEffect(() => {
     /**
-     * Get the three data subarrays and set their respective states.
+     * There are three categories of resident: resident, alumni, guests.
+     * Use their totalCount from the Gatsby page query to see if they have data to render (as of writing there are ZERO "alumni" entries)
+     * - data: the data array from Gatsby containing the first 12 entries of that category
+     * - hasMore: boolean dictating whether there are more entries that can be fetched
+     * - endCursor: starting point for the next fetch of 12 category entries
      */
     const setCategoryData = () => {
-      setResidentsData(data.prismic.residents.edges)
-      setAlumniData(data.prismic.alumni.edges)
-      setGuestsData(data.prismic.guests.edges)
+      /**
+       * Collect the labels of all the resident categories that have entries
+       * Set categoryName to categoryLabels
+       * categoryLabels mapped to make the resident type selection buttons
+       */
+      let labels = []
 
       /**
-       * categoryStatus mapped to make the resident type selection buttons
-       * type: used to name the resident category button
-       * hasData: uses length of each edges data array to set up a ternary return
-       *  - if length > 0: Render a button with the onClick toggle function
-       *  - else: don't render a button for that category
+       * Residents data check
        */
-      setCategoryStatus([
-        {
-          type: 'Residents',
-          hasData: data.prismic.residents.edges.length,
-          count: data.prismic.residents.totalCount,
-        },
-        {
+      if (prismicContent.residents.totalCount) {
+        setResidents({
+          data: prismicContent.residents.edges,
+          hasMore: prismicContent.residents.pageInfo.hasNextPage,
+          endCursor: prismicContent.residents.pageInfo.endCursor,
+        })
+        labels.push('Residents')
+      }
+      /**
+       * Alumni data check
+       */
+      if (prismicContent.alumni.totalCount) {
+        setAlumni({
           type: 'Alumni',
-          hasData: data.prismic.alumni.edges.length,
-          count: data.prismic.alumni.totalCount,
-        },
-        {
+          data: prismicContent.alumni.edges,
+          hasMore: prismicContent.alumni.pageInfo.hasNextPage,
+          endCursor: prismicContent.alumni.pageInfo.endCursor,
+        })
+        labels.push('Alumni')
+      }
+      /**
+       * Guests data check
+       */
+      if (prismicContent.guests.totalCount) {
+        setGuests({
           type: 'Guests',
-          hasData: data.prismic.guests.edges.length,
-          count: data.prismic.guests.totalCount,
-        },
-      ])
+          data: prismicContent.guests.edges,
+          hasMore: prismicContent.guests.pageInfo.hasNextPage,
+          endCursor: prismicContent.guests.pageInfo.endCursor,
+        })
+        labels.push('Guests')
+      }
 
-      setHasMoreRes({
-        hasMore: data.prismic.residents.pageInfo.hasNextPage,
-        endCursor: data.prismic.residents.pageInfo.endCursor,
-      })
+      setCategoryLabels(labels)
     }
 
     return setCategoryData()
   }, [data])
 
+  useEffect(() => {
+    const processFetchedData = () => {
+      /**
+       * Handle a residentFetch update
+       */
+      if (residentFetch) {
+        const { edges, pageInfo } = residentFetch.allResidents
+
+        setResidents({
+          data: [...residents.data, ...edges],
+          hasMore: pageInfo.hasNextPage,
+          endCursor: pageInfo.endCursor,
+        })
+      }
+      /**
+       * Handle an alumniFetch update
+       */
+      if (alumniFetch) {
+        const { edges, pageInfo } = alumniFetch.allResidents
+
+        setResidents({
+          data: [...alumni.data, ...edges],
+          hasMore: pageInfo.hasNextPage,
+          endCursor: pageInfo.endCursor,
+        })
+      }
+      /**
+       * Handle a guestFetch update
+       */
+      if (guestFetch) {
+        const { edges, pageInfo } = guestFetch.allResidents
+
+        setResidents({
+          data: [...guests.data, ...edges],
+          hasMore: pageInfo.hasNextPage,
+          endCursor: pageInfo.endCursor,
+        })
+      }
+    }
+    return processFetchedData()
+  }, [residentFetch, alumniFetch, guestFetch])
+
+  /**
+   * Helper function that toggles the currently display resident category by using the clicked resident category's id
+   */
   function toggleColumn(event) {
     if (isOpen !== event.currentTarget.id) {
       setIsOpen(event.currentTarget.id)
@@ -92,77 +183,180 @@ function ResidentsIndex({ data, prismic }) {
         </header>
         <section className="columns is-mobile is-variable is-2">
           {/* RESIDENT TYPE SELECTOR BUTTONS */}
-          {categoryStatus &&
-            categoryStatus.map(({ type, hasData }, index) =>
-              hasData ? (
-                <Fragment key={`column-${index}-${type}`}>
-                  {/* DESKTOP SIZED BUTTONS */}
-                  <div className="column is-hidden-mobile">
-                    <button
-                      className={
-                        isOpen === type
-                          ? 'button active is-fullwidth is-outlined is-rounded'
-                          : 'button is-fullwidth is-outlined is-rounded'
-                      }
-                      id={type}
-                      onClick={toggleColumn}
-                    >
-                      {type}
-                    </button>
-                  </div>
-                  {/* TOUCH SIZED BUTTONS */}
-                  <div className="column is-hidden-tablet">
-                    <button
-                      className={
-                        isOpen === type
-                          ? 'button is-small active is-fullwidth is-outlined is-rounded'
-                          : 'button is-small is-fullwidth is-outlined is-rounded'
-                      }
-                      id={type}
-                      onClick={toggleColumn}
-                    >
-                      {type}
-                    </button>
-                  </div>
-                </Fragment>
-              ) : null
-            )}
+          {categoryLabels &&
+            categoryLabels.map((type, index) => (
+              <Fragment key={`column-${index}-${type}`}>
+                {/* DESKTOP SIZED BUTTONS */}
+                <div className="column is-hidden-mobile">
+                  <button
+                    className={
+                      isOpen === type
+                        ? 'button active is-fullwidth is-outlined is-rounded'
+                        : 'button is-fullwidth is-outlined is-rounded'
+                    }
+                    id={type}
+                    onClick={toggleColumn}
+                  >
+                    {type}
+                  </button>
+                </div>
+                {/* TOUCH SIZED BUTTONS */}
+                <div className="column is-hidden-tablet">
+                  <button
+                    className={
+                      isOpen === type
+                        ? 'button is-small active is-fullwidth is-outlined is-rounded'
+                        : 'button is-small is-fullwidth is-outlined is-rounded'
+                    }
+                    id={type}
+                    onClick={toggleColumn}
+                  >
+                    {type}
+                  </button>
+                </div>
+              </Fragment>
+            ))}
         </section>
+
         {/* CURRENT HMBK RESIDENTS */}
         {isOpen === 'Residents' ? (
-          <div className="columns is-mobile is-multiline">
-            {residentsData &&
-              residentsData.map(({ node }, index) => {
+          <>
+            <div className="columns is-mobile is-multiline">
+              {residents?.data?.map(({ node }, index) => {
                 return (
                   <SingleResident key={`Resident-${index}`} resident={node} />
                 )
               })}
-          </div>
+            </div>
+            {residents?.hasMore ? (
+              <div className="columns is-mobile">
+                <div className="column">
+                  <button
+                    className="button is-fullwidth is-outlined is-rounded"
+                    onClick={() =>
+                      getMoreResidents({
+                        variables: { after: residents.endCursor },
+                      })
+                    }
+                  >
+                    More Residents!
+                  </button>
+                </div>
+                <div className="column is-narrow">
+                  <a href="#mixes-header">
+                    <button className="button is-fullwidth is-outlined is-rounded">
+                      Top
+                    </button>
+                  </a>
+                </div>
+              </div>
+            ) : (
+              <div className="columns is-mobile">
+                <div className="column is-offset-10 is-2">
+                  <a href="#all-mixes">
+                    <button className="button is-fullwidth is-outlined is-rounded">
+                      Top
+                    </button>
+                  </a>
+                </div>
+              </div>
+            )}
+          </>
         ) : null}
+
         {/* HMBK ALUMNI */}
         {isOpen === 'Alumni' ? (
-          <div className="columns is-mobile is-multiline">
-            {alumniData &&
-              alumniData.map(({ node }, index) => {
+          <>
+            <div className="columns is-mobile is-multiline">
+              {alumni?.data?.map(({ node }, index) => {
                 return (
                   <SingleResident key={`Alumnus-${index}`} resident={node} />
                 )
               })}
-          </div>
+              {alumni?.hasMore ? (
+                <div className="columns is-mobile">
+                  <div className="column">
+                    <button
+                      className="button is-fullwidth is-outlined is-rounded"
+                      onClick={() =>
+                        getMoreAlumni({
+                          variables: { after: alumni.endCursor },
+                        })
+                      }
+                    >
+                      More Alumni
+                    </button>
+                  </div>
+                  <div className="column is-narrow">
+                    <a href="#mixes-header">
+                      <button className="button is-fullwidth is-outlined is-rounded">
+                        Top
+                      </button>
+                    </a>
+                  </div>
+                </div>
+              ) : (
+                <div className="columns is-mobile">
+                  <div className="column is-offset-10 is-2">
+                    <a href="#all-mixes">
+                      <button className="button is-fullwidth is-outlined is-rounded">
+                        Top
+                      </button>
+                    </a>
+                  </div>
+                </div>
+              )}
+            </div>
+          </>
         ) : null}
+
         {/* HMBK GUESTS */}
         {isOpen === 'Guests' ? (
-          <div className="columns is-mobile is-multiline">
-            {guestsData &&
-              guestsData.map(({ node }, index) => {
+          <>
+            <div className="columns is-mobile is-multiline">
+              {guests?.data?.map(({ node }, index) => {
                 return (
                   <SingleResident key={`Guests-${index}`} resident={node} />
                 )
               })}
-          </div>
+            </div>
+            {guests?.hasMore ? (
+              <div className="columns is-mobile">
+                <div className="column">
+                  <button
+                    className="button is-fullwidth is-outlined is-rounded"
+                    onClick={() =>
+                      getMoreGuests({
+                        variables: { after: guests.endCursor },
+                      })
+                    }
+                  >
+                    More Guests
+                  </button>
+                </div>
+                <div className="column is-narrow">
+                  <a href="#mixes-header">
+                    <button className="button is-fullwidth is-outlined is-rounded">
+                      Top
+                    </button>
+                  </a>
+                </div>
+              </div>
+            ) : (
+              <div className="columns is-mobile">
+                <div className="column is-offset-10 is-2">
+                  <a href="#all-mixes">
+                    <button className="button is-fullwidth is-outlined is-rounded">
+                      Top
+                    </button>
+                  </a>
+                </div>
+              </div>
+            )}
+          </>
         ) : null}
 
-        {hasMoreRes ? (
+        {/* {hasMoreRes ? (
           <div className="columns is-mobile">
             <div className="column">
               <button className="button is-fullwidth is-outlined is-rounded">
@@ -187,7 +381,7 @@ function ResidentsIndex({ data, prismic }) {
               </a>
             </div>
           </div>
-        )}
+        )} */}
 
         {/* <pre>{JSON.stringify(data.prismic, null, 2)}</pre> */}
       </div>

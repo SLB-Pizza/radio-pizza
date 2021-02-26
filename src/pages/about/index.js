87b17bc626d3975e1bd83407f8bc6261e7e5af51
Ricardo Dominguez <ricardo@thoughtcatalog.com>
2021-02-25 22:13:58 -0500

    moved helmet out of fragment and into sibling

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -7,7 +7,7 @@ function AboutIndexPage() {
   const { title, description, siteUrl, twitterUsername } = useSiteMetadata()
 
   return (
-    <>
+    <div className="container is-fluid full-height-page">
       <Helmet defer={false}>
         <html lang="en" />
           <title>About {title}</title>
@@ -26,39 +26,37 @@ function AboutIndexPage() {
           <meta name="twitter:site" content={twitterUsername} />
           <meta name="twitter:image" content={`/img/HalfMoon-3.png`} />
       </Helmet>
-      <div className="container is-fluid full-height-page">
-        <div className="columns is-mobile">
-          <div className="column">
-            <div className="content">
-              <p className="title is-size-3">Ears to the concrete.</p>
-              <p className="subtitle is-size-5">
-                Brooklyn based music network connecting music culture across the
-                world through radio, events, and more.
-              </p>
-              <p>
-                Founded in 2017, Half Moon was born out of the need to defy.
-                Essentially, we are destroying mainstream radio and building a
-                platform that allows music and culture to thrive without limits.
-                We pride ourselves in bringing our audience live, uncensored and
-                undiscovered music from a grass-roots perspective reminding the
-                world that the underground is where the sublime lives, grows and
-                becomes timeless.
-              </p>
+      <div className="columns is-mobile">
+        <div className="column">
+          <div className="content">
+            <p className="title is-size-3">Ears to the concrete.</p>
+            <p className="subtitle is-size-5">
+              Brooklyn based music network connecting music culture across the
+              world through radio, events, and more.
+            </p>
+            <p>
+              Founded in 2017, Half Moon was born out of the need to defy.
+              Essentially, we are destroying mainstream radio and building a
+              platform that allows music and culture to thrive without limits.
+              We pride ourselves in bringing our audience live, uncensored and
+              undiscovered music from a grass-roots perspective reminding the
+              world that the underground is where the sublime lives, grows and
+              becomes timeless.
+            </p>
 
-              <p className="display-text is-size-5">Team</p>
-              <p>Surf Allah – Owner & Founder</p>
-              <p>Edrick Chu – Head of Marketing & Partnerships</p>
-              <p className="display-text is-size-5">Contact</p>
-              <p>Business: business@halfmoonbk.com</p>
-              <p>Info: info@halfmoonbk.com</p>
-              <p className="display-text is-size-5">Site</p>
-              <p>Designed by Christian Mejia</p>
-              <p>Programmed by Christian Mejia & Richard Dominguez</p>
-            </div>
+            <p className="display-text is-size-5">Team</p>
+            <p>Surf Allah – Owner & Founder</p>
+            <p>Edrick Chu – Head of Marketing & Partnerships</p>
+            <p className="display-text is-size-5">Contact</p>
+            <p>Business: business@halfmoonbk.com</p>
+            <p>Info: info@halfmoonbk.com</p>
+            <p className="display-text is-size-5">Site</p>
+            <p>Designed by Christian Mejia</p>
+            <p>Programmed by Christian Mejia & Richard Dominguez</p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 

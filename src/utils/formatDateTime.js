578d8062d6fba34317d51ca71d1e4c7d04a27983
Christian Mejia <chrismejia10@gmail.com>
2021-03-04 18:15:28 -0500

    fix(formatDateTime): fix initial parse of Prismic datetime string use `customParseFormat` to properly convert Prismic datetime to EST begin updating cases that handle Prismic date

--- src/utils/formatDateTime.js
+++ src/utils/formatDateTime.js
@@ -1,27 +1,65 @@
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
 dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(customParseFormat)
 
 /**
- * Takes in a Date object or regular date string and returns strings formatted by dayjs
+ * Takes in a Date object or regular date string and returns strings formatted by `dayjs`.
+ *
+ * ## `convertedPrismicDateTime` use cases:
+ *
+ * | `format` value            | Components              |
+ * |---------------------------|-------------------------|
+ * | **"long-form-date"**      | {@link ArticleDateTime} |
+ * | **"long-form-date-time"** | {@link ArticleDateTime}, {@link EventHeader} |
+ * | **"datetime-value"**      | {@link ArticleDateTime} |
  * @category Utilities
  * @function formatDateTime
- * @param {(Object|String)} time - a Date object or regular string containing datetime
+ * @param {Object|String} time - a `dayjs` object or a Prismic datetime string from containing datetime
  * @param {String} format - dictates how to format the incoming time
  * @param {?Number} number - optional number to use when adding to the time param, negative numbers allowed
- * @returns {String}
+ * @returns {String} String time formatting depends on input `format`.
  *
- * @see {@link https://day.js.org/docs/en/manipulate/add dayjs docs - add}
- * @see {@link https://day.js.org/docs/en/display/format dayjs docs - format}
+ * @see {@link https://day.js.org/docs/en/manipulate/add dayjs - add}
+ * @see {@link https://day.js.org/docs/en/display/format dayjs - format}
  */
-
 export default function formatDateTime(time, format, number) {
+  /**
+   * Set up a placeholder variable that'll be used for certain use cases and determine the user's timezone.
+   */
+  let baseDayJSObj
+  const userTimezone = dayjs.tz.guess()
+
+  /**
+   * Convert `time` string to EST in case it comes from Prismic; means it's HMBK-centric.
+   */
+  const convertedPrismicDateTime = dayjs(
+    time,
+    'YYYY-MM-DDTHH:mm:ssZZ',
+    'America/New_York'
+  )
+
+  /**
+   * In the cases we need to format a datetime string in some way, we first need to convert it from UTC to EST.  This value will not always be used; notes made where such cases apply.
+   * @see {@link https://day.js.org/docs/en/plugin/custom-parse-format dayjs - customParseFormat}
+   */
+  if (userTimezone === 'America/New_York') {
+    baseDayJSObj = dayjs(time, 'YYYY-MM-DDTHH:mm:ssZZ', 'America/New_York')
+  } else {
+    baseDayJSObj = dayjs(time, 'YYYY-MM-DDTHH:mm:ssZZ', userTimezone)
+  }
+
+  /**
+
+   */
   switch (format) {
+    case 'current-time':
+      return dayjs().tz('America/New_York')
     case 'add-days':
       return time.add(number, 'day').format('MM.DD')
-    case 'UTC-to-EST':
-      // event_start and event_end come in as UTC time, so subtract 4 hours
-      return time.subtract(4, 'hours')
     case 'prismic-date-query':
       return dayjs(time)
         .add(number, 'day')
@@ -37,12 +75,18 @@ export default function formatDateTime(time, format, number) {
     case 'schedule-date-heading':
       return dayjs(time).format('dddd, MMMM D')
     case 'long-form-date-time':
-      return dayjs(time).format('MMMM D, YYYY - HH:mm')
+      return convertedPrismicDateTime.format('MMMM D, YYYY - HH:mm')
     case 'long-form-date':
-      return dayjs(time).format('MMMM D, YYYY')
+      return convertedPrismicDateTime.format('MMMM D, YYYY')
     case 'datetime-value':
-      return dayjs(time).format('YYYY-MM-DD HH:mm:ssZ')
+      return convertedPrismicDateTime.format('YYYY-MM-DD HH:mm:ssZ')
+    case 'time-debug':
+      console.log(
+        dayjs(time, 'YYYY-MM-DDTHH:mm:ssZZ').format('MMMM D, YYYY - HH:mm Z')
+      )
+      return dayjs(time, 'YYYY-MM-DDTHH:mm:ssZZ').isValid()
     default:
+      // Return a current Date object
       return dayjs()
   }
 }

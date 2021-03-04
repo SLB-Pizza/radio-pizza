cb210bdfcc092ca4da9c83289282b6a5911ebaf6
Christian Mejia <chrismejia10@gmail.com>
2021-03-04 18:28:09 -0500

    refactor(scheduling): add base docs fix in-map props processing remove commented code create layout helpers for ScheduleEntryRow

--- src/components/ScheduleDropdown.js
+++ src/components/ScheduleDropdown.js
@@ -8,7 +8,17 @@ dayjs.extend(utc)
 import { SingleScheduleEntryRow } from '../components'
 import dummySchedule from '../../__test__/HMBK-schedule-page-query-test.json'
 
-// See ScheduleShowEntry for details on these
+/**
+ * Renders the schedule dropdown bar when `setOpen` is set to `true` in {@link ScheduleBar}.
+ * @category Site Elements
+ * @function ScheduleDropdown
+ * @param {} setOpen,
+ * @param {} open
+ * @param {} toggleSchedule
+ * @param {} showData
+ * @param {} timeNow
+ * @returns {jsx}
+ */
 function ScheduleDropdown({
   setOpen,
   open,
@@ -16,26 +26,17 @@ function ScheduleDropdown({
   showData,
   timeNow,
 }) {
-  // const [todayDate, setTodayDate] = useState(dayjs());
-
-  // useEffect(() => {
-  //   const date = setInterval(() => {
-  //     setTodayDate(todayDate.add(1, "s"));
-  //   }, 5000);
-
-  //   return () => {
-  //     clearInterval(date);
-  //   };
-  // });
-
   const { schedule_date, schedule_entries } = showData
 
+  /**
+   * Format date using {@link formatDateTime}.
+   */
   const todaysDate = formatDateTime(timeNow, 'schedule-date-heading')
 
   return (
     <div className="columns is-multiline is-vcentered is-mobile dropdown">
       <div className="column">
-        <p className="display-text is-size-5-desktop is-size-6-touch has-text-centered">
+        <p className="title is-size-5-desktop is-size-6-touch has-text-centered">
           {todaysDate}
         </p>
       </div>
@@ -43,7 +44,7 @@ function ScheduleDropdown({
       <div className="column is-narrow">
         <Link to="/schedule">
           <button
-            className="button is-small is-outlined is-rounded display-text"
+            className="button is-small is-outlined is-rounded"
             onClick={() => {
               setOpen(!open)
               toggleSchedule()
@@ -54,22 +55,16 @@ function ScheduleDropdown({
         </Link>
       </div>
 
-      {/* 
+      {/*
       schedule_entries !== undefined : display today's schedule
       schedule_entries === undefined : show  */}
       {schedule_entries !== undefined ? (
         <div className="column is-12">
           {schedule_entries.map((entry, index) => {
-            const { start_time, end_time, scheduled_show } = entry
-            const formattedStart = formatDateTime(start_time, 'hour-minute')
-            const formattedEnd = formatDateTime(end_time, 'hour-minute')
-
             return (
               <SingleScheduleEntryRow
                 key={`show-entry-#${index}-${start_time}`}
-                start={formattedStart}
-                end={formattedEnd}
-                show={scheduled_show}
+                entry={entry}
               />
             )
           })}
@@ -81,33 +76,6 @@ function ScheduleDropdown({
           </p>
         </div>
       )}
-      {/* {fakeShowEntryData.map((show) => (
-        <div key={show.hostInfo} className="column is-12 single-show-entry">
-          <div className="columns is-mobile is-vcentered">
-            <div className="column is-4">
-              <p className="title is-size-6-tablet is-size-7-mobile has-text-centered">
-                {show.startTime} – {show.endTime}
-              </p>
-            </div>
-            {show.hasOwnProperty("showName") ? (
-              <div className="column is-8">
-                <p className="display-text is-size-6-tablet is-size-7-mobile has-text-centered">
-                  {show.showName}
-                </p>
-                <p className="display-text is-size-7 has-text-centered">
-                  {show.hostInfo.join(", ")}
-                </p>
-              </div>
-            ) : (
-              <div className="column is-8">
-                <p className="display-text is-size-6-tablet is-size-7-mobile has-text-centered">
-                  {show.hostInfo.join(", ")}
-                </p>
-              </div>
-            )}
-          </div>
-        </div>
-      ))} */}
     </div>
   )
 }
--- src/components/SingleScheduleEntryRow.js
+++ src/components/SingleScheduleEntryRow.js
@@ -2,38 +2,73 @@ import React from 'react'
 import { getResidentLinks } from '../utils'
 
 /**
+ * Renders a single schedule row, containing the start and end times, as well as the name of the radio show.
  * @category Site Elements
-
  * @function SingleScheduleEntryRow
- * @param {Object} props
+ * @param {Object.<String, String|Object>} entry - the data object for a single scheduled radio show
  * @returns {jsx}
  */
-function SingleScheduleEntryRow({ start, end, show }) {
-  const { mix_title, featured_residents } = show
+function SingleScheduleEntryRow({ entry }) {
+  const { start_time, end_time, scheduled_show } = entry
+  const { mix_title, featured_residents } = scheduled_show
+
+  const ShowTimes = () => {
+    const formattedStart = formatDateTime(start_time, 'hour-minute')
+    const formattedEnd = formatDateTime(end_time, 'hour-minute')
+
+    if (start_time && end_time) {
+      // Render both the formatted start and end times.
+      return (
+        <div className="column is-4">
+          <p className="title is-size-6-tablet is-size-7-mobile has-text-centered">
+            {formattedStart} – {formattedEnd}
+          </p>
+        </div>
+      )
+    } else if (start_time && !end_time) {
+      // Only render the formatted start time.
+      return (
+        <div className="column is-4">
+          <p className="title is-size-6-tablet is-size-7-mobile has-text-centered">
+            {formattedStart}
+          </p>
+        </div>
+      )
+    } else {
+      // Render a fallback indicating that neither a start time nor an end time have been set.
+      return (
+        <div className="column is-4">
+          <p className="title is-size-6-tablet is-size-7-mobile has-text-centered">
+            --:-- – --:--
+          </p>
+        </div>
+      )
+    }
+  }
+
+  const RenderRadioShowName = () => {
+    return mix_title !== null ? (
+      <div className="column is-8">
+        <p className="title is-size-6-tablet is-size-7-mobile has-text-centered">
+          {mix_title}
+        </p>
+        <p className="subtitle is-size-7 has-text-centered">
+          {getResidentLinks(featured_residents)}
+        </p>
+      </div>
+    ) : (
+      <div className="column is-8">
+        <p className="title is-size-6-tablet is-size-7-mobile has-text-centered">
+          {getResidentLinks(featured_residents)}
+        </p>
+      </div>
+    )
+  }
 
   return (
     <div className="columns is-mobile is-vcentered single-show-entry">
-      <div className="column is-4">
-        <p className="title is-size-6-tablet is-size-7-mobile has-text-centered">
-          {start} – {end}
-        </p>
-      </div>
-      {mix_title !== null ? (
-        <div className="column is-8">
-          <p className="title is-size-6-tablet is-size-7-mobile has-text-centered">
-            {mix_title}
-          </p>
-          <p className="subtitle is-size-7 has-text-centered">
-            {getResidentLinks(featured_residents)}
-          </p>
-        </div>
-      ) : (
-        <div className="column is-8">
-          <p className="title is-size-6-tablet is-size-7-mobile has-text-centered">
-            {getResidentLinks(featured_residents)}
-          </p>
-        </div>
-      )}
+      <ShowTimes />
+      <RenderRadioShowName />
     </div>
   )
 }

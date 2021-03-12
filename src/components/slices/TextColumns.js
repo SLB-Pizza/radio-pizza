0920192e512663158da7d64ed584c9dbcc57c3cd
Christian Mejia <chrismejia10@gmail.com>
2021-03-12 17:16:56 -0500

    feat(TextColumns): change slice render condition TextColumns will not render is `filteredTextCols` is returned as 0 docs updated #117 - short-circuit TextColumns

--- src/components/slices/TextColumns.js
+++ src/components/slices/TextColumns.js
@@ -3,7 +3,7 @@ import { RichTextHelper } from '../helpers'
 import { mappableDataFilter } from '../../utils'
 
 /**
- * Renders a Prismic RichText structured text object. One of the instances of `.text-block`, a class name that changes line height and link rendering.
+ * Renders a set of Prismic RichText columns, with optional header and footer sections. One of the instances of `.text-block`, a class name that changes line height and link rendering. This slice will NOT render if `filteredTextCols` is returned by {@link mappableDataFilter} to be 0.
  * @category CMS Slices
  * @function TextColumns
  * @param {Object} slice - data object from Prismic CMS that contains all content data needed to create the HeadlineBlock slice
@@ -15,34 +15,33 @@ function TextColumns({ slice }) {
 
   const filteredTextCols = mappableDataFilter(fields)
 
-  console.log(filteredTextCols)
-
-  return (
-    <section className="section slice text-columns debug">
+  return filteredTextCols ? (
+    <section className="section slice text-columns">
       {header && (
         <div className="container">
           <div className="columns text-columns__header">
+            return
             <RichTextHelper richText={header} />
           </div>
         </div>
       )}
-      {filteredTextCols && (
-        <div className="container is-fluid">
-          <div className="columns is-mobile is-centered">
-            <div className="column is-11">
-              <div className="columns is-mobile is-multiline text-columns__main">
-                {filteredTextCols.map(({ text_column }, index) => (
-                  <RichTextHelper
-                    key={`text-column-${index}`}
-                    richText={text_column}
-                    columnSizing={'column text-column'}
-                  />
-                ))}
-              </div>
+
+      <div className="container is-fluid">
+        <div className="columns is-mobile is-centered">
+          <div className="column is-11">
+            <div className="columns is-mobile is-multiline text-columns__main">
+              {filteredTextCols.map(({ text_column }, index) => (
+                <RichTextHelper
+                  key={`text-column-${index}`}
+                  richText={text_column}
+                  columnSizing={'column text-column'}
+                />
+              ))}
             </div>
           </div>
         </div>
-      )}
+      </div>
+
       {footer && (
         <div className="container text-columns__footer">
           <div className="columns is-mobile is-multiline">
@@ -51,7 +50,7 @@ function TextColumns({ slice }) {
         </div>
       )}
     </section>
-  )
+  ) : null
 }
 
 export default TextColumns

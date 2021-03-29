fac9175c36455c40377da326e4f1fffb04dc0758
Christian Mejia <chrismejia10@gmail.com>
2021-03-28 22:22:42 -0400

    refactor(queries): update getTodaysSchedule import

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,6 +1,6 @@
 export { FILL_HOME_MIXES } from './fillHomeMixes'
 export { FILL_HOME_FEATURES } from './fillHomeFeatures'
-export { GET_NEXT_SHOW } from './getNextShow'
+export { GET_TODAYS_SCHEDULE } from './getTodaysSchedule'
 export { GET_DEFAULT_MIX } from './getDefaultMix'
 export { GET_SELECTED_TAGGED_MIXES } from './getSelectedTaggedMixes'
 export { GET_SEVEN_DAY_SCHEDULE } from './getSevenDaySchedule'

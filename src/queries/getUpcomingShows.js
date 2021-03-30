04b79e7a0933e7aeddd7a18353a4230487ea42c9
Christian Mejia <chrismejia10@gmail.com>
2021-03-30 05:55:49 -0400

    refactor(query): rename query; expand to 2 results; update exports

--- src/queries/getUpcomingShows.js
+++ src/queries/getUpcomingShows.js
@@ -2,19 +2,20 @@ import { gql } from '@apollo/client'
 
 /**
  * Query for Prismic in the GraphQL syntax, not the Gatsby syntax!
- * Retrieves the first available date after yesterday with scheduled show entries.
+ * Retrieves the first two available dates after yesterday with scheduled show entries.
+ * Fetch 2 to cover case where current time is after end of last scheduled show today; present the next available show on a date.
  * Used in {@link ScheduleBar}.
  * @category Queries
- * @name GET_TODAYS_SCHEDULE
+ * @name GET_UPCOMING_SHOWS
  * @see {@link https://hmbk-cms.prismic.io/graphql HMBK's Prismic GraphQL API}
  * @see {@link https://prismic.io/docs/graphql/query-the-api/query-by-date Prismic - GraphQL Query by Date}
  */
-export const GET_TODAYS_SCHEDULE = gql`
-  query getTodaysSchedule($yesterday: Date!) {
+export const GET_UPCOMING_SHOWS = gql`
+  query getUpcomingShows($yesterday: Date!) {
     allSchedules(
       where: { schedule_date_after: $yesterday }
       sortBy: schedule_date_ASC
-      first: 1
+      first: 2
     ) {
       edges {
         node {

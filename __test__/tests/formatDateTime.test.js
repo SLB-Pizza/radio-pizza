e321e2216861d82c22206c48951a1cd748cfed95
Christian Mejia <chrismejia10@gmail.com>
2021-03-30 21:07:17 -0400

    test(formatDateTime): fix broken tests add more test cases

--- __test__/tests/formatDateTime.test.js
+++ __test__/tests/formatDateTime.test.js
@@ -12,9 +12,7 @@ dayjs.extend(isBetween)
 dayjs.extend(isSameOrBefore)
 dayjs.extend(customParseFormat)
 
-dayjs('2010-10-20').isSameOrBefore('2010-10-19', 'year')
-
-describe.only('formatDateTime', () => {
+describe('formatDateTime', () => {
   describe("'current-time'", () => {
     it('returns a valid dayjs instance', () => {
       const time = formatDateTime(null, 'current-time')
@@ -90,26 +88,123 @@ describe.only('formatDateTime', () => {
      * > now
      * -- START first show today
      */
-    describe('returns "now live"', () => {
-      const time = formatDateTime(null, 'current-time')
+    const time = formatDateTime(null, 'current-time')
 
-      it('when current time is between a show start and end time', () => {
-        const fiveMinsBefore = dayjs(time)
-          .add(-5, 'minute')
-          .format('HH:mm')
-        const fiveMinsAfter = dayjs(time)
-          .add(5, 'minute')
-          .format('HH:mm')
+    it("returns `true` when current time is between a show's start and end time", () => {
+      const fiveMinsBefore = dayjs(time).add(-5, 'minute')
+      const fiveMinsAfter = dayjs(time).add(5, 'minute')
+
+      const timeIsBetween = formatDateTime(
+        time,
+        'get-place-in-schedule',
+        null,
+        fiveMinsBefore,
+        fiveMinsAfter
+      )
+
+      expect(timeIsBetween).to.be.true
+    })
+
+    describe('returns `false`', () => {
+      it("when current time is before a show's start time", () => {
+        const fifteenMinsBefore = dayjs(time).add(-15, 'minute')
+        const tenMinsBefore = dayjs(time).add(-10, 'minute')
 
         const timeIsBetween = formatDateTime(
           time,
           'get-place-in-schedule',
           null,
-          fiveMinsBefore,
-          fiveMinsAfter
+          fifteenMinsBefore,
+          tenMinsBefore
         )
 
-        expect(timeIsBetween).to.be.true
+        expect(timeIsBetween).to.be.false
+
+        const twoDaysMinus10Before = tenMinsBefore.add(-2, 'day')
+        const twoDaysMinus15Before = fifteenMinsBefore.add(-2, 'day')
+
+        const timeIsDaysBefore = formatDateTime(
+          time,
+          'get-place-in-schedule',
+          null,
+          twoDaysMinus10Before,
+          twoDaysMinus15Before
+        )
+
+        expect(timeIsDaysBefore).to.be.false
+      })
+
+      it("when current time is after a show's start time", () => {
+        const tenMinsAfter = dayjs(time).add(10, 'minute')
+        const fifteenMinsAfter = dayjs(time).add(15, 'minute')
+
+        const timeIsMinutesAfter = formatDateTime(
+          time,
+          'get-place-in-schedule',
+          null,
+          tenMinsAfter,
+          fifteenMinsAfter
+        )
+
+        expect(timeIsMinutesAfter).to.be.false
+
+        const twoDaysPlus10After = tenMinsAfter.add(2, 'day')
+        const twoDaysPlus15After = fifteenMinsAfter.add(2, 'day')
+
+        const timeIsDaysAfter = formatDateTime(
+          time,
+          'get-place-in-schedule',
+          null,
+          twoDaysPlus10After,
+          twoDaysPlus15After
+        )
+
+        expect(timeIsDaysAfter).to.be.false
+      })
+    })
+  })
+
+  describe('is-schedule-date-today', () => {
+    const time = formatDateTime(null, 'current-time')
+
+    it('returns `true` when date to match is today', () => {
+      const today = time.format('YYYY-MM-DD')
+
+      const dateIsToday = formatDateTime(
+        time,
+        'is-schedule-date-today',
+        null,
+        today
+      )
+
+      expect(dateIsToday).to.be.true
+    })
+
+    describe('returns `false`', () => {
+      it('when day is before today', () => {
+        const yesterday = time.add(-1, 'day').format('YYYY-MM-DD')
+
+        const dateIsToday = formatDateTime(
+          time,
+          'is-schedule-date-today',
+          null,
+          yesterday
+        )
+
+        expect(dateIsToday).to.be.false
+      })
+
+      it('when day is after today', () => {
+        const tomorrow = time.add(1, 'day').format('YYYY-MM-DD')
+
+        const dateIsToday = formatDateTime(
+          time,
+          'is-schedule-date-today',
+          null,
+          tomorrow
+        )
+
+        expect(dateIsToday).to.be.false
       })
     })
   })

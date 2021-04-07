20a509262cda7982c7a79ef9786f41d2f3618312
Christian Mejia <chrismejia10@gmail.com>
2021-04-07 17:46:11 -0400

    docs(measureTextWidth): update

--- src/utils/measureTextWidth.js
+++ src/utils/measureTextWidth.js
@@ -8,8 +8,16 @@
  * @see {@link https://jsfiddle.net/eNzjZ/70/ JSFiddle: Measuring Text Width - Canvas vs jQuery}
  */
 export default function measureTextWidth() {
+  /**
+   * Grab the `.upcoming-show` div's width
+   */
   const upcomingShowWidth = document.querySelector('.upcoming-show').clientWidth
 
+  /**
+   * Focus the `<p>` tag within `.upcoming-show`
+   * Grab its text output and font-size
+   * Create a styling string to pass to canvas text
+   */
   const element = document.querySelector('.upcoming-show p')
   const text = element.innerText
   const fontSize = window
@@ -17,10 +25,14 @@ export default function measureTextWidth() {
     .getPropertyValue('font-size')
   const canvasFontStyle = `${fontSize} Poppins`
 
+  /**
+   * Grab the dummy canvas
+   * Set its font-styling
+   * Measure the `<p>` tag text using the canvas, round result down.
+   */
   const canvas = document.getElementById('upcoming-measure')
   const context = canvas.getContext('2d')
   context.font = canvasFontStyle
-  context.width = upcomingShowWidth
   const textWidth = Math.floor(context.measureText(text).width)
 
   /**

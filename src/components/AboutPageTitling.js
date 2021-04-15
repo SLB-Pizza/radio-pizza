74130200a5cf798cc26d50f2af16b11b40e8ba55
Christian Mejia <chrismejia10@gmail.com>
2021-04-14 22:29:08 -0400

    fix(/about): adjust logo sizing and titling columns

--- src/components/AboutPageTitling.js
+++ src/components/AboutPageTitling.js
@@ -14,19 +14,19 @@ export default function AboutPageTitling({ aboutTitling }) {
 
   return (
     <section className="container about-titling">
-      <div className="columns is-centered is-vcentered">
+      <div className="columns is-mobile is-centered is-vcentered">
         <div className="column is-narrow">
-          <figure className="image is-128x128">
+          <figure className="image is-96x96">
             <FallbackImage styleName={'lazyload'} />
           </figure>
         </div>
         <div className="column is-narrow">
-          <h1 className="title is-size-1-widescreen is-size-2-desktop is-size-3-touch">
+          <h1 className="title is-size-1-widescreen is-size-2-desktop is-size-3-tablet is-size-5-mobile">
             {article_headline
               ? RichText.asText(article_headline)
               : 'HalfmoonBK'}
           </h1>
-          <p className="subtitle is-size-3-widescreen is-size-4-desktop is-size-5-touch">
+          <p className="subtitle is-size-3-widescreen is-size-4-desktop is-size-5-tablet is-size-7-mobile">
             {article_subtitle
               ? RichText.asText(article_subtitle)
               : 'Ears to the concrete.'}

3f2cbb5498e9a91bf397ac347c0a1640dc39b1ad
Christian Mejia <chrismejia10@gmail.com>
2021-04-28 20:15:46 -0400

    refactor(AdminLinkButtons): import CMSLink component

--- src/components/admin/AdminLinkButtons.js
+++ src/components/admin/AdminLinkButtons.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SingleAdminLink } from './index'
+import { AdminCMSLink, SingleAdminLink } from './index'
 
 /**
  * Renders the `/hmbk-admin` links section.
@@ -14,22 +14,27 @@ export default function AdminLinkButtons() {
         icon={'info-circle'}
         linkPath={'netlify-status'}
         linkText={'View Netlify Status'}
+        linkDescription={'Details about current and recent deploys'}
       />
       <SingleAdminLink
         icon={'broadcast-tower'}
         linkPath={'live-stream-info'}
         linkText={'Update Live Broadcast Info'}
+        linkDescription={'Edit the details for live shows'}
       />
       <SingleAdminLink
         icon={'book'}
         linkPath={'guides'}
         linkText={'Read HalfmoonBK Guides'}
+        linkDescription={'Editorial standards, media rules, and more'}
       />
       <SingleAdminLink
         icon={'calendar-alt'}
         linkPath={'full-schedule'}
         linkText={'View Complete Schedule'}
+        linkDescription={''}
       />
+      <AdminCMSLink />
     </div>
   )
 }

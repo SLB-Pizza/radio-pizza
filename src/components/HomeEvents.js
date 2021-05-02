97ad9b699137e0834df6471b449e8a44733ffbe7
Christian Mejia <chrismejia10@gmail.com>
2021-05-02 19:40:59 -0400

    refactor(HomeEvents): call StickyItemsLayout and pass props

--- src/components/HomeEvents.js
+++ src/components/HomeEvents.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import { SingleEventCard } from './index'
+import { SingleEventCard, StickyItemsLayout } from './index'
 
 /**
  * Returns the Events content section of the Homepage.
@@ -11,61 +10,20 @@ import { SingleEventCard } from './index'
  * @param {Object[]} homeEventsData - Array of data from Prismic received from /index; original data set in Prismic Homepage document
  * @returns {jsx}
  */
-function HomeEvents({ blurb, headline, homeEventsData }) {
+export default function HomeEvents({ blurb, headline, homeEventsData }) {
   const eventPageLayout = 'column is-9-mobile is-two-fifths-tablet is-4-desktop'
 
   return (
-    <div className="container is-fluid" id="home-events">
-      {/* DESKTOP */}
-      <div className="columns is-hidden-touch">
-        <div className="column is-3">
-          <div className="sticky-section content">
-            {headline && <h3 className="title">{headline}</h3>}
-            {blurb && <p className="subtitle">{blurb}</p>}
-            <Link to="/events">
-              <button className="button is-outlined is-rounded">
-                All Events
-              </button>
-            </Link>
-          </div>
-        </div>
-        <div className="column is-9">
-          <div className="columns is-multiline">
-            {homeEventsData?.map(({ node }, index) => (
-              <SingleEventCard
-                key={`halfmoon-event-${index}`}
-                data={node}
-                columnLayout={eventPageLayout}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
-      {/* TOUCH */}
-      <div className="columns is-mobile is-multiline is-vcentered is-hidden-desktop">
-        <div className="column">
-          {headline && <h3 className="title is-4">{headline}</h3>}
-          {blurb && <p className="subtitle is-6">{blurb}</p>}
-        </div>
-        <div className="column is-narrow">
-          <Link to="/events">
-            <button className="button is-small is-outlined is-rounded">
-              All Events
-            </button>
-          </Link>
-        </div>
-      </div>
-      <div className="columns is-mobile is-hidden-desktop mobile-single-items">
-        {homeEventsData?.map(({ node }, index) => (
-          <SingleEventCard
-            key={`halfmoon-event-${index}`}
-            data={node}
-            columnLayout={eventPageLayout}
-          />
-        ))}
-      </div>
-    </div>
+    homeEventsData && (
+      <StickyItemsLayout
+        headline={headline}
+        blurb={blurb}
+        linkURL={'/events'}
+        linkBtnText={'All Events'}
+        itemsToMap={homeEventsData}
+        ItemComponent={SingleEventCard}
+        layout={eventPageLayout}
+      />
+    )
   )
 }
-
-export default HomeEvents
--- src/components/HomeMixes.js
+++ src/components/HomeMixes.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'gatsby'
 import { useLazyQuery } from '@apollo/client'
 import { SingleMixCard, StickyItemsLayout } from './index'
 import { FILL_HOME_MIXES } from '../queries'

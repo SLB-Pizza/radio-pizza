abec024f924f953622e5fcea233aecbb1771cd27
Christian Mejia <chrismejia10@gmail.com>
2021-05-02 19:36:42 -0400

    refactor(HomeMixes): same refactor as HomeFeatures add optional useLazyQuery call filter out fetchedMixes by UID call StickyItemsLayout and pass processed twelve mixes

--- src/components/HomeMixes.js
+++ src/components/HomeMixes.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'gatsby'
-import { useQuery } from '@apollo/client'
-
-import { SingleMixCard } from './index'
+import { useLazyQuery } from '@apollo/client'
+import { SingleMixCard, StickyItemsLayout } from './index'
 import { FILL_HOME_MIXES } from '../queries'
-import { mappableDataFilter } from '../utils'
+import {
+  getUIDsFromDataArray,
+  mappableDataFilter,
+  removeDuplicateFetchData,
+} from '../utils'
 
 /**
  * Returns the Mixes content section of the Homepage, directly underneath the {@link Hero} section.
@@ -15,65 +18,86 @@ import { mappableDataFilter } from '../utils'
  * @param {Object[]} homeMixesData - Array of data from Prismic received from /index; original data set in Prismic Homepage document
  * @returns {jsx}
  */
-function HomeMixes({ headline, blurb, homeMixesData }) {
-  const [twelveMixes, setTwelveMixes] = useState(null)
+export default function HomeMixes({ headline, blurb, homeMixesData }) {
+  const [twelveMixes, setHomeMixes] = useState(null)
 
   const filteredHomeMixes = mappableDataFilter(homeMixesData)
-  const mixQueryCount = 12 - filteredHomeMixes.length
 
   /**
-   * Uses the query {@link FILL_HOME_MIXES} to fetch `mixQueryCount` number of mixes to set twelvesMixes to map.
-   * @category useQuery
-   * @name FILL_HOME_MIXES
+   * Uses the query {@link FILL_HOME_FEATURES} to fetch 4 Features for {@link processFetchedHomeMixes} to work with.
+   * @category useLazyQuery
+   * @name HomeMixesQuery
    */
-  const { data, loading, error } = useQuery(FILL_HOME_MIXES, {
-    variables: {
-      count: mixQueryCount,
-    },
-  })
+  const [fetchFillerHomeMixes, { data: fetchedMixes, loading }] = useLazyQuery(
+    FILL_HOME_MIXES
+  )
 
   /**
-   * Three scenarios:
-   * 1. homeMixesData has more than 12 mix objects
-   * Grab only the first 12 mixes
-   * 2. homeMixesData has less than 12 mix objects
-   * Subtract quantity of filteredHomeMixes from 12
-   * Query for that many of the most recent mixes
-   * Spread that data into the filteredHomeMixes array
-   * setTwelveMixes the new 12 mix filteredHomeMixes
-   * 3. homeMixesData has exactly 12 mix objects
-   * we have exactly 12 mixes; directly setTwelveMixes off it
+   * IF `filteredHomeMixes` has 4 or more entries
+   *    Grab only the first 4 features
+   * ELSE
+   *    `filteredHomeMixes` has less than 4 entries
+   *    Fetch 4 Features by calling {@link HomeMixesQuery}.
+   *    `fetchedMixes` updates, triggering {@link processFetchedHomeMixes}.
    * @category useEffect
    * @name fetchRemainingHomeMixes
    */
   useEffect(() => {
     const fetchRemainingHomeMixes = () => {
-      if (filteredHomeMixes.length > 12) {
+      if (filteredHomeMixes.length >= 12) {
         /**
          * Scenario 1
          */
         const mixesToMap = filteredHomeMixes.slice(0, 13)
         setTwelveMixes(mixesToMap)
-      } else if (filteredHomeMixes.length < 12) {
-        /**
-         * Scenario 2
-         */
-
-        if (data) {
-          const fetchedRecentMixes = data.allMixs.edges
-          const newTwelveMixes = [...filteredHomeMixes, ...fetchedRecentMixes]
-          setTwelveMixes(newTwelveMixes)
-        }
       } else {
-        /**
-         * Scenario 3
-         */
-        setTwelveMixes(filteredHomeMixes)
+        fetchFillerHomeMixes()
       }
     }
 
     return fetchRemainingHomeMixes()
-  }, [homeMixesData, data])
+  }, [homeMixesData])
+
+  /**
+   * Runs when {@link fetchFillerHomeFeatures} returns fetchedFeatures to process.
+   * Scenario 1
+   * We have exactly 4 features
+   *
+   * Scenario 2
+   * Query 4 more recently published Features.
+   * Spread that fetchedFeatures into the filteredHomeMixes array
+   * setTwelveMixes the new 12 mix filteredHomeMixes
+   * @category useEffect
+   * @name processFetchedHomeMixes
+   */
+  useEffect(() => {
+    const processFetchedHomeMixes = () => {
+      if (!fetchedMixes) {
+        /**
+         * Scenario 1
+         */
+        setHomeMixes(filteredHomeMixes)
+      } else {
+        /**
+         * Scenario 2
+         */
+        const mixUIDsToFilter = getUIDsFromDataArray(filteredHomeMixes)
+        const fetchedRecentMixes = fetchedMixes.allMixs.edges
+
+        const uidFilteredRecentFeatures = removeDuplicateFetchData(
+          fetchedRecentMixes,
+          mixUIDsToFilter
+        )
+
+        const twelveMixesToMap = [
+          ...filteredHomeMixes,
+          ...uidFilteredRecentFeatures,
+        ]
+        setHomeMixes(twelveMixesToMap)
+      }
+    }
+    processFetchedHomeMixes()
+  }, [fetchedMixes])
 
   /**
    * The string passed into {@link SingleMixCard} that defines the column sizing for the mix cards in the Mixes section of the Homepage.
@@ -82,57 +106,16 @@ function HomeMixes({ headline, blurb, homeMixesData }) {
   const homeMixesLayout = 'column is-9-mobile is-two-fifths-tablet is-4-desktop'
 
   return (
-    <section className="section container is-fluid" id="home-mixes">
-      {/* DESKTOP */}
-      <div className="columns is-hidden-touch">
-        <div className="column is-3">
-          <div className="sticky-section content">
-            {headline && <h3 className="title">{headline}</h3>}
-            {blurb && <p className="subtitle">{blurb}</p>}
-            <Link to="/radio">
-              <button className="button is-outlined is-rounded">
-                All Radio
-              </button>
-            </Link>
-          </div>
-        </div>
-        <div className="column is-9">
-          <div className="columns is-multiline">
-            {twelveMixes?.map(({ node }, index) => (
-              <SingleMixCard
-                key={`mixes-page-#${index}`}
-                data={node}
-                columnLayout={homeMixesLayout}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
-      {/* TOUCH */}
-      <div className="columns is-mobile is-multiline is-vcentered is-hidden-desktop">
-        <div className="column">
-          {headline && <h3 className="title is-4">{headline}</h3>}
-          {blurb && <p className="subtitle is-6">{blurb}</p>}
-        </div>
-        <div className="column is-narrow">
-          <Link to="/radio">
-            <button className="button is-small is-outlined is-rounded">
-              All Radio
-            </button>
-          </Link>
-        </div>
-      </div>
-      <div className="columns is-mobile is-hidden-desktop mobile-single-items">
-        {twelveMixes?.map(({ node }, index) => (
-          <SingleMixCard
-            key={`mixes-page-#${index}`}
-            data={node}
-            columnLayout={homeMixesLayout}
-          />
-        ))}
-      </div>
-    </section>
+    twelveMixes && (
+      <StickyItemsLayout
+        headline={headline}
+        blurb={blurb}
+        linkURL={'/radio'}
+        linkBtnText={'All Radio'}
+        itemsToMap={twelveMixes}
+        ItemComponent={SingleMixCard}
+        layout={homeMixesLayout}
+      />
+    )
   )
 }
-
-export default HomeMixes

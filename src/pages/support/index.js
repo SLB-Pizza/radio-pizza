db98258ba4035f0a829428767d92c5e675b398be
Christian Mejia <chrismejia10@gmail.com>
2021-05-04 20:56:13 -0400

    remove(/support): render blank page

--- src/pages/support/index.js
+++ src/pages/support/index.js
@@ -11,34 +11,35 @@ import PropTypes from 'prop-types'
  * @param {object} data - the data object coming from Prismic CMS that contains all data needed to build the `/features` landing page
  * @returns {jsx}
  */
-function SupportPage({ data }) {
+export default function SupportPage() {
+  // function SupportPage({data}) {
   /**
    * Focus the node for the prismicContent check below.
    */
-  const prismicContent = data.prismic.allSupports.edges[0]
+  // const prismicContent = data.prismic.allSupports.edges[0];
 
   /**
    * This line is here to prevent an error from occurring when you eventually deploy the site live. There is an issue with the preview functionality that requires this check on every page.
    * @see https://prismic.io/docs/gatsby/rendering/retrieve-the-document-object#21_0-adding-a-validation-check
    */
-  if (!prismicContent) return null
+  // if (!prismicContent) return null;
 
   /**
    * The data from the 'EditorialIndexPage' query comes pre-sorted to show the most recent published feature, NOT the most recently updated.
    *
    * The remaining array of node objects can be mapped over normally using XYZ_Component.
    */
-  const supportPageData = prismicContent.node
+  // const supportPageData = prismicContent.node;
 
   /**
    * Pull out parts from supportPageData to make passing data easier.
    */
-  const supportHeader = {
-    cta: supportPageData.support_cta,
-    hook: supportPageData.support_cta_hook,
-    img: supportPageData.support_cta_bg_img,
-  }
-  const supportSlice = supportPageData.body
+  // const supportHeader = {
+  //   cta: supportPageData.support_cta,
+  //   hook: supportPageData.support_cta_hook,
+  //   img: supportPageData.support_cta_bg_img,
+  // };
+  // const supportSlice = supportPageData.body;
 
   return (
     <main className="full-height-page">
@@ -48,7 +49,7 @@ function SupportPage({ data }) {
         imgObj={supportHeader.img}
       />
       <SliceZone sliceZone={supportSlice} /> */}
-      <hr />
+      {/* <hr />
 
       <section className="container is-fluid">
         <div className="columns is-multiline">
@@ -57,43 +58,41 @@ function SupportPage({ data }) {
             <pre>{JSON.stringify(supportSlice, null, 2)}</pre>
           </div>
         </div>
-      </section>
+      </section> */}
     </main>
   )
 }
 
-export default SupportPage
+// SupportPage.propTypes = {
+//   leadFeatureData: PropTypes.exact({
+//     _meta: PropTypes.object.isRequired,
+//     body: PropTypes.arrayOf(PropTypes.object),
+//   }),
+//   allOtherFeatures: PropTypes.arrayOf(PropTypes.object),
+// };
 
-SupportPage.propTypes = {
-  leadFeatureData: PropTypes.exact({
-    _meta: PropTypes.object.isRequired,
-    body: PropTypes.arrayOf(PropTypes.object),
-  }),
-  allOtherFeatures: PropTypes.arrayOf(PropTypes.object),
-}
-
-export const query = graphql`
-  query SupportPage {
-    prismic {
-      allSupports {
-        edges {
-          node {
-            support_cta
-            support_cta_bg_img
-            support_cta_hook
-            body {
-              ... on PRISMIC_SupportBodyOne_image_and_text {
-                type
-                primary {
-                  oiat_layout
-                  oiat_text
-                  oiat_img
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query SupportPage {
+//     prismic {
+//       allSupports {
+//         edges {
+//           node {
+//             support_cta
+//             support_cta_bg_img
+//             support_cta_hook
+//             body {
+//               ... on PRISMIC_SupportBodyOne_image_and_text {
+//                 type
+//                 primary {
+//                   oiat_layout
+//                   oiat_text
+//                   oiat_img
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `

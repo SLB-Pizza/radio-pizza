eca3c9691c9a986bce395cb4c86c51f4137cedc7
Christian Mejia <chrismejia10@gmail.com>
2021-06-11 16:56:36 -0400

    fix(RadioBar): remove setInitialSource useEffect remove GET_DEFAULT_MIX useQuery led to possible race conditions when setting initial RadioPlayerDisplay data BUG: Depending on how fast you hit the play button after load, you'd get the live broadcast; but the recorded stuff would display

--- src/components/RadioBar.js
+++ src/components/RadioBar.js
@@ -1,10 +1,8 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'gatsby'
-import { useQuery } from '@apollo/client'
 import { RadioPlayer } from './index'
 import { FallbackImage } from '../utils'
-import { formatDateTime, getResidentString, mappableDataFilter } from '../utils'
-import { GET_DEFAULT_MIX } from '../queries'
+import { formatDateTime } from '../utils'
 import {
   GlobalDispatchContext,
   GlobalStateContext,
@@ -26,85 +24,8 @@ export default function RadioBar({ nycTime, laTime }) {
   const dispatch = useContext(GlobalDispatchContext)
   const globalState = useContext(GlobalStateContext)
 
-  /**
-   * @category useQuery
-   * @name getDefaultMix
-   */
-  const { loading, error, data } = useQuery(GET_DEFAULT_MIX)
   const clearMixSearchTags = async () =>
     await dispatch({ type: 'CLEAR_MIX_SEARCH_TAGS' })
-
-  /**
-   * Query the HMBK Prismic CMS to get the data for the initial mix data.
-   * Grab the mix data object from the query result.
-   * Destructure the mix data object and dispatch the mix data to appear in {@link RadioPlayer}
-   * @category useEffect
-   * @name setInitialSource
-   */
-  useEffect(() => {
-    const setInitialSource = async () => {
-      if (globalState.live) {
-        await dispatch({
-          type: 'SET_INITIAL_RADIO',
-        })
-      } else {
-        if (loading) {
-          // console.log("Initial Mix request in progress");
-        }
-        if (error) {
-          console.error(`initialMix Error: ${error.message}`)
-        }
-        if (data) {
-          const mixDataObject = data.allTopnavs.edges[0].node.default_mix
-
-          if (mixDataObject) {
-            const {
-              featured_residents,
-              mix_image,
-              mix_link,
-              mix_title,
-            } = mixDataObject
-
-            /**
-             * Another case where `objectKeyCount` needs to be assigned `2` for {@link mappableDataFilter}; `__typename` counts!.
-             */
-            const filteredResidents = mappableDataFilter(featured_residents, 2)
-            const mixResidentsString = getResidentString(filteredResidents)
-
-            await dispatch({
-              type: 'SET_INITIAL_MIX',
-              payload: {
-                url: mix_link,
-                title: mix_title,
-                resident: mixResidentsString,
-                img: mix_image.now_playing.url,
-              },
-            })
-            await dispatch({
-              type: 'MIX_LOADED',
-            })
-          } else {
-            /**
-             * Handle case where no initial mix is selected in the CMS.
-             */
-            await dispatch({
-              type: 'SET_INITIAL_MIX',
-              payload: {
-                url: null,
-                title: null,
-                resident: null,
-                img: null,
-              },
-            })
-            await dispatch({
-              type: 'MIX_LOADED',
-            })
-          }
-        }
-      }
-    }
-    setInitialSource()
-  }, [data, loading, error])
 
   return (
     <div className="container is-fluid radio-bar">

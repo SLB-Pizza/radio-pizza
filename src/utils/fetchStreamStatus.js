f97b44267fc4f3e7211b89e5785f6566b98f6696
Christian Mejia <chrismejia10@gmail.com>
2021-06-14 18:27:31 -0400

    fix(fetchStreamStatus): add more and method to fetch

--- src/utils/fetchStreamStatus.js
+++ src/utils/fetchStreamStatus.js
@@ -3,12 +3,13 @@
  * Called by {@link ScheduleBar} to set initial TopNav status and to poll for radio live updates.
  * @category Utilities
  * @function fetchStreamStatus
- * @returns {Boolean}
+ * @returns {String} One of "online" or "offline"
  */
 export default async function fetchStreamStatus() {
   try {
     const streamResponse = await fetch(
-      `https://public.radio.co/stations/s6f093248d/status`
+      `https://public.radio.co/stations/s6f093248d/status`,
+      { method: 'GET', mode: 'cors' }
     )
     const data = await streamResponse.json()
     return data.status

d9c1c0614ac7e321effad5a6ccf19416d0b6701c
Christian Mejia <chrismejia10@gmail.com>
2021-06-16 17:11:59 -0400

    fix(helmetTitling): add case where both liveShow title and guests exist

--- src/utils/setHelmetSiteTitling.js
+++ src/utils/setHelmetSiteTitling.js
@@ -1,19 +1,22 @@
 /**
- * Processes `globalState` to derive the site's title, (in the tab), using HMBK's radio.co status, and the default mix set in the CMS.
+ * Processes `globalState` to derive the site's title, (in the tab), using HMBK's radio.co status, and the default mix set in the CMS. Reflects the site title text on {@link IndexPage} only; all other landings and pages have their own titling overrides.
  *
- * `isLoading` is `true`
- *    return the default titling
- *
- * IF site is `live`
+ * IF `isLoading` is `true`
+ *    'Half Moon | Ears to the Concrete'
+ * ELSE IF site is `live`
  *    IF the player is playing
  *      start titling with "LIVE"
  *    ELSE
  *      start titling with "ON AIR"
  *
  *    IF `liveShowTitle` exists
+ *      IF `liveShowGuests` also exists
+ *        append `liveShowTitle - liveShowGuests | Half Moon`
+ *      ELSE
+ *        append `liveShowTitle | Half Moon`
  *      append it to titling
  *    ELSE
- *      append `liveShowGuests` to titling
+ *      append `liveShowGuests | Half Moon`
  *    NB: Because of how {@link submitMarquee} prevents
  *    `liveShowTitle` and `liveShowGuests` from both being null `onSubmit`,
  *    at least one of the two is always guaranteed to exists.
@@ -34,13 +37,13 @@
  */
 export default function setHelmetSiteTitling(globalState) {
   const { isLoading, live, liveMarquee, playing, title, resident } = globalState
+  const { liveShowTitle, liveShowGuests } = liveMarquee
 
   /**
    * The default titling string to use:
    * - if `isLoading` is true
    * - as the ultimate fallback if all tested conditions fail
    */
-
   let titling = 'Half Moon | Ears to the Concrete'
 
   if (isLoading) {
@@ -56,11 +59,15 @@ export default function setHelmetSiteTitling(globalState) {
       // Add on the live titling
       // Use the set broadcast show titling
       // ELSE use the guest string, if all that's set
-      // No double null case needed,
-      if (liveMarquee.liveShowTitle) {
-        titling += `${liveMarquee.liveShowTitle} | Half Moon`
+      // No double null case needed, handled by default
+      if (liveShowTitle) {
+        if (liveShowGuests) {
+          titling += `${liveShowTitle} - ${liveShowGuests} | Half Moon`
+        } else {
+          titling += `${liveShowTitle} | Half Moon`
+        }
       } else {
-        titling += `${liveMarquee.liveShowGuests} | Half Moon`
+        titling += `${liveShowGuests} | Half Moon`
       }
     }
     // Use title and resident string together when both are present
@@ -75,7 +82,8 @@ export default function setHelmetSiteTitling(globalState) {
     } else if (resident) {
       titling = `${resident} | Half Moon`
     }
-    // If HMBK is NOT live and there is no title OR resident to use
+    // IF   HMBK is NOT live
+    // AND  There is no title OR resident to use
     // the original titling will be returned
   }
   return titling
